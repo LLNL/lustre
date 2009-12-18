d395ef5aa14d800ccee7c7f81b2528e9bada286a
Christopher J. Morrone <morrone2@llnl.gov>
2009-12-17 16:25:17 -0800

    b=18066 Tune fsfilt slow disk transaction warning
    Sun bug 18066, attachment 20976
    
    Increase threshold for 'slow' disk transaction warnings to the console.
    By default this is set to 30 seconds which is far to low.  On hype/igs
    with 500+ service threads per OSS and a heavy load its not uncommon to
    see 100+ second service delays.  We only want to see these warning on
    the console when things are moving very slowly, these errors will still
    go to the internal debug log.
    
    We should use INFO/WARN instead of WARN/ERROR for the slow messages.
    Not only is there no real error here but it fixes an annoying quirk
    of the message formatting.  With the old levels you would see the
    messages formatted differently based on the time.
    
      Lustre: lc1-OST0001: slow parent lock 289s due to heavy IO load
      LustreError: 0-0: lc1-OST0001: slow parent lock 324s due to heavy IO load
    
    With the new levels things are more consistent.
    
      Lustre: lc1-OST0001: slow parent lock 289s due to heavy IO load
      Lustre: lc1-OST0001: slow parent lock 324s due to heavy IO load
    
    Change-Id: Ieae13875f7baf32ad7d17661cedf4ec3e2f92320
    Signed-off-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Signed-off-by: Christopher J. Morrone <morrone2@llnl.gov>

--- lustre/include/obd_support.h
+++ lustre/include/obd_support.h
@@ -127,7 +127,7 @@ int obd_alloc_fail(const void *ptr, const char *name, const char *type,
  * and there's no urgent need to evict a client just because it's idle, we
  * should be very conservative here. */
 #define PING_EVICT_TIMEOUT (PING_INTERVAL * 6)
-#define DISK_TIMEOUT 50          /* Beyond this we warn about disk speed */
+#define DISK_TIMEOUT 300         /* Beyond this we warn about disk speed */
 #define CONNECTION_SWITCH_MIN 5U /* Connection switching rate limiter */
  /* Max connect interval for nonresponsive servers; ~50s to avoid building up
     connect requests in the LND queues, but within obd_timeout so we don't
