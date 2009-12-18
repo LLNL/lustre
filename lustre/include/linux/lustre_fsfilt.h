0b917a339fce68058d3f19edc7cc6bda6cd36b59
Christopher J. Morrone <morrone2@llnl.gov>
2009-12-17 16:25:17 -0800

    b=18066 Tune fsfilt slow disk transaction warning
    Sun bug 18066, attachment 20976
    
    Increase threshold for 'slow' disk transaction warnings to the console.
    By default this is set to 30 seconds which is far to low.  On hype/igs
    with 500+ service threads per OSS and a heavy load its not uncommon to
    see 100+ second service delays.  We only want to see these warning on
    the console when things are moving very slowly, these errors will still
    go to the internal debug log.
    
    We should use INFO/WARN instead of WARN/ERROR for the slow messages.
    Not only is there no real error here but it fixes an annoying quirk
    of the message formatting.  With the old levels you would see the
    messages formatted differently based on the time.
    
      Lustre: lc1-OST0001: slow parent lock 289s due to heavy IO load
      LustreError: 0-0: lc1-OST0001: slow parent lock 324s due to heavy IO load
    
    With the new levels things are more consistent.
    
      Lustre: lc1-OST0001: slow parent lock 289s due to heavy IO load
      Lustre: lc1-OST0001: slow parent lock 324s due to heavy IO load
    
    Change-Id: Ieae13875f7baf32ad7d17661cedf4ec3e2f92320
    Signed-off-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Signed-off-by: Christopher J. Morrone <morrone2@llnl.gov>

--- lustre/include/linux/lustre_fsfilt.h
+++ lustre/include/linux/lustre_fsfilt.h
@@ -194,7 +194,7 @@ static inline lvfs_sbdev_type fsfilt_journal_sbdev(struct obd_device *obd,
 do {                                                                      \
         if (cfs_time_before(jiffies, start + 15 * CFS_HZ))                \
                 break;                                                    \
-        else if (cfs_time_before(jiffies, start + 30 * CFS_HZ))           \
+        else if (cfs_time_before(jiffies, start + DISK_TIMEOUT / 2 * CFS_HZ)) \
                 CDEBUG(D_VFSTRACE, "%s: slow %s %lus\n", obd->obd_name,   \
                        msg, (jiffies-start) / CFS_HZ);                    \
         else if (cfs_time_before(jiffies, start + DISK_TIMEOUT * CFS_HZ)) \
