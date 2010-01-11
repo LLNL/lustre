633089313af31cfe4f217eabcefa51b840042bf0
Joseph Herring <herring2@llnl.gov>
2010-01-11 14:48:09 -0800

    b=21665 lfs osts tries to traverse lustre mount
    Attachment 27814

--- lustre/utils/liblustreapi.c
+++ lustre/utils/liblustreapi.c
@@ -1325,11 +1325,13 @@ static int cb_ostlist(char *path, DIR *parent, DIR *d, void *data,
                        struct dirent64 *de)
 {
         struct find_param *param = (struct find_param *)data;
+        int ret;
 
         LASSERT(parent != NULL || d != NULL);
 
         /* Prepare odb. */
-        return setup_obd_uuid(d ? d : parent, path, param);
+        ret = setup_obd_uuid(d ? d : parent, path, param);
+        return ret == 0 ? 1 : ret;
 }
 
 int llapi_ostlist(char *path, struct find_param *param)
