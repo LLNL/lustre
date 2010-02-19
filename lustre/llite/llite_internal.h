33c383ab6b458fa640e41f539e26f04250b70b0c
Brian Behlendorf <behlendorf1@llnl.gov>
2010-02-19 13:06:41 -0800

    b=16909 Standardize and optionally suppress mount/umount messages
    Standardize mount/umount console message to include profile name,
    and optionally suppress them with the 'quiet' mount option.  We
    have been using private namespaces for testing and mounting then
    umounting the FS as needed for each job.  In this context these
    messages end up causing alot of syslog noise.

--- lustre/llite/llite_internal.h
+++ lustre/llite/llite_internal.h
@@ -303,6 +303,7 @@ enum stats_track_type {
 #define LL_SBI_LRU_RESIZE       0x80 /* support lru resize */
 #define LL_SBI_LLITE_CHECKSUM  0x100 /* checksum each page in memory */
 #define LL_SBI_LAZYSTATFS      0x200 /* lazystatfs mount option */
+#define LL_SBI_VERBOSE         0x400 /* verbose mount/umount */
 
 /* default value for ll_sb_info->contention_time */
 #define SBI_DEFAULT_CONTENTION_SECONDS     60
