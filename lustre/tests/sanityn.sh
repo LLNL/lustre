99dc31a1e1861f63f518f252ce5c20f07af7e739
Mikhail Pershin <tappro@whamcloud.com>
2011-01-30 11:44:50 +0300

    ORI-75 Add test 15 to the except list due to oos2.sh bug
    Change-Id: I091fc557e9030324d8ec52eb9223cde7488f9e79
    Signed-off-by: Mikhail Pershin <tappro@whamcloud.com>

--- lustre/tests/sanityn.sh
+++ lustre/tests/sanityn.sh
@@ -7,9 +7,12 @@ ONLY=${ONLY:-"$*"}
 ALWAYS_EXCEPT="                14b  19         22    28   29          35    $SANITYN_EXCEPT"
 # UPDATE THE COMMENT ABOVE WITH BUG NUMBERS WHEN CHANGING ALWAYS_EXCEPT!
 
-# bug number for skipped test:        12652 12652
-grep -q 'Enterprise Server 10' /etc/SuSE-release 2> /dev/null &&
-	ALWAYS_EXCEPT="$ALWAYS_EXCEPT 11    14" || true
+# Orion exceptions
+# 15 - oos2.sh is broken
+ALWAYS_EXCEPT="15 $ALWAYS_EXCEPT"
+
+# bug number for skipped test:                                                    12652 12652
+grep -q 'Enterprise Server 10' /etc/SuSE-release && ALWAYS_EXCEPT="$ALWAYS_EXCEPT 11    14" || true
 
 # Tests that fail on uml
 [ "$UML" = "true" ] && EXCEPT="$EXCEPT 7"
