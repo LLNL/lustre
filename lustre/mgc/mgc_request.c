c54c7d7af054a950976ad8f1f37ba12f44aff3af
Mikhail Pershin <tappro@whamcloud.com>
2011-03-22 20:04:24 +0300

    ORI-66 Remove llog_lvfs.c and llog context from mgc
    Change-Id: I62d9a3bf1d815fcb431a1403f2985f175394669c
    Signed-off-by: Mikhail Pershin <tappro@whamcloud.com>

--- lustre/mgc/mgc_request.c
+++ lustre/mgc/mgc_request.c
@@ -954,27 +954,14 @@ static int mgc_llog_init(struct obd_device *obd, struct obd_llog_group *olg,
 
         LASSERT(olg == &obd->obd_olg);
 
-        rc = llog_setup(obd, olg, LLOG_CONFIG_ORIG_CTXT, tgt, 0, NULL,
-                        &llog_lvfs_ops);
-        if (rc)
-                RETURN(rc);
-
         rc = llog_setup(obd, olg, LLOG_CONFIG_REPL_CTXT, tgt, 0, NULL,
                         &llog_client_ops);
         if (rc == 0) {
                 ctxt = llog_get_context(obd, LLOG_CONFIG_REPL_CTXT);
-                if (!ctxt) {
-                        ctxt = llog_get_context(obd, LLOG_CONFIG_ORIG_CTXT);
-                        if (ctxt)
-                                llog_cleanup(ctxt);
+                if (!ctxt)
                         RETURN(-ENODEV);
-                }
                 llog_initiator_connect(ctxt);
                 llog_ctxt_put(ctxt);
-        } else {
-                ctxt = llog_get_context(obd, LLOG_CONFIG_ORIG_CTXT);
-                if (ctxt)
-                        llog_cleanup(ctxt);
         }
 
         RETURN(rc);
@@ -983,19 +970,12 @@ static int mgc_llog_init(struct obd_device *obd, struct obd_llog_group *olg,
 static int mgc_llog_finish(struct obd_device *obd, int count)
 {
         struct llog_ctxt *ctxt;
-        int rc = 0, rc2 = 0;
+        int rc = 0;
         ENTRY;
 
         ctxt = llog_get_context(obd, LLOG_CONFIG_REPL_CTXT);
         if (ctxt)
                 rc = llog_cleanup(ctxt);
-
-        ctxt = llog_get_context(obd, LLOG_CONFIG_ORIG_CTXT);
-        if (ctxt)
-                rc2 = llog_cleanup(ctxt);
-
-        if (!rc)
-                rc = rc2;
 
         RETURN(rc);
 }
