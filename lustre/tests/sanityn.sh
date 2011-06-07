6119663abfd29fee890fe3a6863da8c9a636562c
Alex Zhuravlev <bzzz@whamcloud.com>
2011-06-07 20:59:14 +0400

    ORI-120 sanityn/36: FAIL: space leake
     sync data from client's cache
    
    Change-Id: Ic125331461adb7da1e04f80e00280477c1b35b25
    Signed-off-by: Alex Zhuravlev <bzzz@whamcloud.com>

--- lustre/tests/sanityn.sh
+++ lustre/tests/sanityn.sh
@@ -907,40 +907,34 @@ test_36() { #bug 16417
 	local SIZE_B
 	local i
 
-	mkdir -p $DIR1/$tdir
-	$LFS setstripe -c -1 $DIR1/$tdir
-	i=0
-	SIZE=50
-	let SIZE_B=SIZE*1024*1024
+    mkdir -p $DIR1/$tdir
+    $LFS setstripe -c -1 $DIR1/$tdir
+    i=0
+    SIZE=50
+    let SIZE_B=SIZE*1024*1024
 
-	while [ $i -le 10 ]; do
-		lctl mark "start test"
-		local before=$($LFS df | awk '{if ($1 ~/^filesystem/) \
-		                               {print $5; exit} }')
-		dd if=/dev/zero of=$DIR1/$tdir/file000 bs=1M count=$SIZE
-		sync          # sync data from client's cache
-		sync_all_data # sync data from server's cache (delayed
-		              # allocation)
-		sleep 1
-		local after_dd=$($LFS df | awk '{if ($1 ~/^filesystem/) \
-		                                 {print $5; exit} }')
-		multiop_bg_pause $DIR2/$tdir/file000 O_r${SIZE_B}c || return 3
-		read_pid=$!
-		rm -f $DIR1/$tdir/file000
-		kill -USR1 $read_pid
-		wait $read_pid
-		sleep 1
-		local after=$($LFS df | awk '{if ($1 ~/^filesystem/) \
-		                              {print $5; exit} }')
-		echo "*** cycle($i) *** before($before) after_dd($after_dd)" \
-			"after($after)"
-		# this free space! not used
-		if [ $after_dd -ge $after ]; then
-			error "space leaked"
-			return 1;
-		fi
-		let i=i+1
-	done
+    while [ $i -le 10 ]; do
+        lctl mark "start test"
+        local before=$($LFS df | awk '{if ($1 ~/^filesystem/) {print $5; exit} }')
+        dd if=/dev/zero of=$DIR1/$tdir/file000 bs=1M count=$SIZE
+        sync		# sync data from client's cache
+        sync_all_data	# sync data from server's cache (delayed allocation)
+        local after_dd=$($LFS df | awk '{if ($1 ~/^filesystem/) {print $5; exit} }')
+        multiop_bg_pause $DIR2/$tdir/file000 O_r${SIZE_B}c || return 3
+        read_pid=$!
+        rm -f $DIR1/$tdir/file000
+        kill -USR1 $read_pid
+        wait $read_pid
+        wait_delete_completed
+        local after=$($LFS df | awk '{if ($1 ~/^filesystem/) {print $5; exit} }')
+        echo "*** cycle($i) *** before($before):after_dd($after_dd):after($after)"
+        # this free space! not used
+        if [ $after_dd -ge $after ]; then
+            error "space leaked"
+            return 1;
+        fi
+        let i=i+1
+            done
 }
 run_test 36 "handle ESTALE/open-unlink corectly"
 
