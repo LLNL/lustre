77121bcb0bb6d0a63597d7089fa467861a68702b
Alex Zhuravlev <bzzz@whamcloud.com>
2011-06-15 13:48:52 +0400

    additional debug: error() in t-f to dump osp status
    Change-Id: I788642ee957f2b3aaf60847991b12568a70c8724
    Signed-off-by: Alex Zhuravlev <bzzz@whamcloud.com>

--- lustre/tests/test-framework.sh
+++ lustre/tests/test-framework.sh
@@ -2987,6 +2987,7 @@ error() {
     # specific lod/osp bits: check space available and in-progress deletes
     lfs df
     do_nodes $(mdts_nodes) "lctl get_param osp.*.sync_*"
+    do_nodes $(mdts_nodes) "lctl get_param osp.*.prealloc_status"
     error_noexit "$@"
     exit 1
 }
