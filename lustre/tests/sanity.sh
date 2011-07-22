733b2e5f9fec6de687eb3abb293b2dbb5b3be387
Johann Lombardi <johann@whamcloud.com>
2011-07-22 16:49:29 +0200

    ORI-216 remove useless call to wait_delete_completed
    The problem has been fixed by 2c09ccc3b3bc9c0c374c908a6432b773fe716eaa.
    
    Change-Id: I40f14229f5ec416d606cec4224a00115d45c5e12
    Signed-off-by: Johann Lombardi <johann@whamcloud.com>
    Reviewed-on: http://review.whamcloud.com/1133

--- lustre/tests/sanity.sh
+++ lustre/tests/sanity.sh
@@ -3774,7 +3774,6 @@ run_test 64a "verify filter grant calculations (in kernel) ====="
 
 test_64b () {
 	[ ! -f oos.sh ] && skip_env "missing subtest oos.sh" && return
-	wait_delete_completed
 	sh oos.sh $MOUNT
 }
 run_test 64b "check out-of-space detection on client ==========="
