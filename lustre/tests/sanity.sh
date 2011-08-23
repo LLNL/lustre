23a1a0d5600f32c697be5e379b4e2791b665fec8
Alex Zhuravlev <bzzz@whamcloud.com>
2011-08-23 14:02:20 +0400

    ORI-241 return default striping for root directory only
    Change-Id: Ib5e873284e571ea68782fafcb7b84dd72ae3ed91
    Signed-off-by: Alex Zhuravlev <bzzz@whamcloud.com>
    Reviewed-on: http://review.whamcloud.com/1277
    Tested-by: Hudson
    Reviewed-by: Mikhail Pershin <tappro@whamcloud.com>

--- lustre/tests/sanity.sh
+++ lustre/tests/sanity.sh
@@ -25,10 +25,9 @@ ALWAYS_EXCEPT="$ALWAYS_EXCEPT 76"
 
 # Orion: to be fixed
 # 160 -- (bug 22448) changelogs don't work yet
-# 204e - ORI-241
 # 124a - LU-479 is not yet merged
 # 225 - md_echo needs fixes
-ALWAYS_EXCEPT="$ALWAYS_EXCEPT 124a 160 204e 225"
+ALWAYS_EXCEPT="$ALWAYS_EXCEPT 124a 160 225"
 
 case `uname -r` in
 2.6*) FSTYPE=${FSTYPE:-ldiskfs} ;;
