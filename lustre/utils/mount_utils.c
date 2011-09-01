c18e4efd4aa60e2112c4b4a5122694f6a76c3fff
Brian Behlendorf <behlendorf1@llnl.gov>
2011-09-01 14:50:18 -0700

    ORI-294 osd-zfs refactor mkfs/tunefs/mount
    Refactor the mkfs.lustre, tunefs.lustre, mount.lustre utilities
    to cleanly support multiple backend filesystems.  While this
    commit initially appears large it does not introduce any
    functional changes.  It does however do the following.
    
    * Adds generic osd_* helper functions which allows us to move
      the backend specific code in to a osd_<backend>.c files.
      Each supported backend must implement the following hooks.
    
      * osd_write_ldd      - Write the contents of the lustre_disk_data
                             structure safely to the backend filesystem.
      * osd_read_ldd       - Read the contents of the lustre_disk_data
                             structure back from the backend filesystem.
      * osd_is_lustre      - Determine if the passed device is being
                             used as a Lustre server, and if it is
                             what type of backend is being used.
      * osd_prepare_lustre - Prepare for making the backend filesystem.
                             This can include setting up loop back
                             devices, handling mount options, etc.
      * osd_make_lustre    - Create the backend filesystem for Lustre.
      * osd_tune_lustre    - Tune the backend filesystem for Lustre.
    
    * Global variables consolidated in mkfs_opts/mount_opts
      structures so this information may be cleanly passed to
      common functions provided by mount_util.c.
    
    * Functions which were defined once in each utility have
      been made common and consolidated in mount_utils.c.  In
      a few cases the common function was made mode generic.
    
    * Split mkfs.lustre and tunefs.lustre in seperate .c files
      for clarity.  This removes the need for the TUNEFS #define.
    
    * General cleanup.  This includes removing unused variables
      and function, renaming functions for consistently, and
      removing unused headers.
    
    The bulk of this ldiskfs specific code was simply moved from the
    existing utilties and consolidated in mount_utils_ldiskfs.c.
    Minimal changes were made to support the refactored infrastructure.
    
    A small fix was also made to the lustre spec file.  It was
    mistakenly checking if the libiam.c file exists and then assuming
    it was safe to add the libiam.a library.  However, the library
    will only be build when the the ldiskfs osd code is enabled so
    it should check for the .a file.
    
    Finally, the --nomgs options was removed from conf-sanity test 24a
    because it doesn't make sense as a mkfs option.  It was silently
    ignored in the past, but with the seperation of mkfs and tunefs
    it now generates an unknown option error.
    
    Change-Id: I65d8f2b514445e1771f68ce243592ffc7561c430
    Signed-off-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Reviewed-on: http://review.whamcloud.com/1357
    Tested-by: Hudson
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>

--- lustre/utils/mount_utils.c
+++ lustre/utils/mount_utils.c
@@ -35,25 +35,19 @@
  * Lustre is a trademark of Sun Microsystems, Inc.
  */
 
-#if HAVE_CONFIG_H
-#  include "config.h"
-#endif /* HAVE_CONFIG_H */
-
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
 #include <config.h>
-#include <lustre_disk.h>
-#include <lustre_ver.h>
 #include <sys/stat.h>
 #include <sys/utsname.h>
+
+#include <libcfs/libcfs.h>
+#include <lustre_disk.h>
+#include <lustre_param.h>
+#include <lustre_ver.h>
+
 #include "mount_utils.h"
-
-extern char *progname;
-extern int verbose;
-
-#define vprint(fmt, arg...) if (verbose > 0) printf(fmt, ##arg)
-#define verrprint(fmt, arg...) if (verbose >= 0) fprintf(stderr, fmt, ##arg)
 
 void fatal(void)
 {
@@ -127,85 +121,60 @@ int run_command(char *cmd, int cmdsz)
         return run_command_err(cmd, cmdsz, NULL);
 }
 
-static int readcmd(char *cmd, char *buf, int len)
+int add_param(char *buf, char *key, char *val)
 {
-        FILE *fp;
-        int red;
+        int end = sizeof(((struct lustre_disk_data *)0)->ldd_params);
+        int start = strlen(buf);
+        int keylen = 0;
 
-        fp = popen(cmd, "r");
-        if (!fp)
-                return errno;
+        if (key)
+                keylen = strlen(key);
+        if (start + 1 + keylen + strlen(val) >= end) {
+                fprintf(stderr, "%s: params are too long-\n%s %s%s\n",
+                        progname, buf, key ? key : "", val);
+                return 1;
+        }
 
-        red = fread(buf, 1, len, fp);
-        pclose(fp);
-
-        /* strip trailing newline */
-        if (buf[red - 1] == '\n')
-                buf[red - 1] = '\0';
-
-        return (red == 0) ? -ENOENT : 0;
+        sprintf(buf + start, " %s%s", key ? key : "", val);
+        return 0;
 }
 
-int get_mountdata(char *dev, struct lustre_disk_data *mo_ldd)
+int get_param(char *buf, char *key, char **val)
 {
+        int i, key_len = strlen(key);
+        char *ptr;
 
-        char tmpdir[] = "/tmp/lustre_tmp.XXXXXX";
-        char cmd[256];
-        char filepnm[128];
-        FILE *filep;
-        int ret = 0;
-        int ret2 = 0;
-        int cmdsz = sizeof(cmd);
+        ptr = strstr(buf, key);
+        if (ptr) {
+                *val = strdup(ptr + key_len);
+                if (*val == NULL)
+                        return ENOMEM;
 
-        /* Make a temporary directory to hold Lustre data files. */
-        if (!mkdtemp(tmpdir)) {
-                verrprint("%s: Can't create temporary directory %s: %s\n",
-                        progname, tmpdir, strerror(errno));
-                return errno;
+                for (i = 0; i < strlen(*val); i++)
+                        if (((*val)[i] == ' ') || ((*val)[i] == '\0'))
+                                break;
+
+                (*val)[i] = '\0';
+                return 0;
         }
 
-        snprintf(cmd, cmdsz, "%s -c -R 'dump /%s %s/mountdata' %s",
-                 DEBUGFS, MOUNT_DATA_FILE, tmpdir, dev);
+        return ENOENT;
+}
 
-        ret = run_command(cmd, cmdsz);
-        if (ret) {
-                verrprint("%s: Unable to dump %s from %s (%d)\n",
-                          progname, MOUNT_DATA_FILE, dev, ret);
-                goto out_rmdir;
+char *strscat(char *dst, char *src, int buflen) {
+        dst[buflen - 1] = 0;
+        if (strlen(dst) + strlen(src) >= buflen) {
+                fprintf(stderr, "string buffer overflow (max %d): '%s' + '%s'"
+                        "\n", buflen, dst, src);
+                exit(EOVERFLOW);
         }
+        return strcat(dst, src);
 
-        sprintf(filepnm, "%s/mountdata", tmpdir);
-        filep = fopen(filepnm, "r");
-        if (filep) {
-                size_t num_read;
-                vprint("Reading %s\n", MOUNT_DATA_FILE);
-                num_read = fread(mo_ldd, sizeof(*mo_ldd), 1, filep);
-                if (num_read < 1 && ferror(filep)) {
-                        fprintf(stderr, "%s: Unable to read from file (%s): %s\n",
-                                progname, filepnm, strerror(errno));
-                        goto out_close;
-                }
-        } else {
-                verrprint("%s: Unable to open temp data file %s\n",
-                          progname, filepnm);
-                ret = 1;
-                goto out_rmdir;
-        }
+}
 
-out_close:
-        fclose(filep);
-
-out_rmdir:
-        snprintf(cmd, cmdsz, "rm -rf %s", tmpdir);
-        ret2 = run_command(cmd, cmdsz);
-        if (ret2)
-                verrprint("Failed to remove temp dir %s (%d)\n", tmpdir, ret2);
-
-        /* As long as we at least have the label, we're good to go */
-        snprintf(cmd, sizeof(cmd), E2LABEL" %s", dev);
-        ret = readcmd(cmd, mo_ldd->ldd_svname, sizeof(mo_ldd->ldd_svname) - 1);
-
-        return ret;
+char *strscpy(char *dst, char *src, int buflen) {
+        dst[0] = 0;
+        return strscat(dst, src, buflen);
 }
 
 /* Convert symbolic hostnames to ipaddrs, since we can't do this lookup in the
@@ -254,3 +223,472 @@ char *convert_hostnames(char *s1)
         return converted;
 }
 
+#undef getmntent
+int check_mtab_entry(char *spec1, char *spec2, char *mtpt, char *type)
+{
+        FILE *fp;
+        struct mntent *mnt;
+
+        fp = setmntent(MOUNTED, "r");
+        if (fp == NULL)
+                return 0;
+
+        while ((mnt = getmntent(fp)) != NULL) {
+                if ((strcmp(mnt->mnt_fsname, spec1) == 0 ||
+                     strcmp(mnt->mnt_fsname, spec2) == 0) &&
+                    (mtpt == NULL || strcmp(mnt->mnt_dir, mtpt) == 0) &&
+                    (type == NULL || strcmp(mnt->mnt_type, type) == 0)) {
+                        endmntent(fp);
+                        return(EEXIST);
+                }
+        }
+        endmntent(fp);
+
+        return 0;
+}
+
+int update_mtab_entry(char *spec, char *mtpt, char *type, char *opts,
+                  int flags, int freq, int pass)
+{
+        FILE *fp;
+        struct mntent mnt;
+        int rc = 0;
+
+        mnt.mnt_fsname = spec;
+        mnt.mnt_dir = mtpt;
+        mnt.mnt_type = type;
+        mnt.mnt_opts = opts ? opts : "";
+        mnt.mnt_freq = freq;
+        mnt.mnt_passno = pass;
+
+        fp = setmntent(MOUNTED, "a+");
+        if (fp == NULL) {
+                fprintf(stderr, "%s: setmntent(%s): %s:",
+                        progname, MOUNTED, strerror (errno));
+                rc = 16;
+        } else {
+                if ((addmntent(fp, &mnt)) == 1) {
+                        fprintf(stderr, "%s: addmntent: %s:",
+                                progname, strerror (errno));
+                        rc = 16;
+                }
+                endmntent(fp);
+        }
+
+        return rc;
+}
+
+/*
+ * Search for opt in mntlist, returning true if found.
+ */
+static int in_mntlist(char *opt, char *mntlist)
+{
+        char *ml, *mlp, *item, *ctx = NULL;
+
+        if (!(ml = strdup(mntlist))) {
+                fprintf(stderr, "%s: out of memory\n", progname);
+                exit(1);
+        }
+        mlp = ml;
+        while ((item = strtok_r(mlp, ",", &ctx))) {
+                if (!strcmp(opt, item))
+                        break;
+                mlp = NULL;
+        }
+        free(ml);
+        return (item != NULL);
+}
+
+/*
+ * Issue a message on stderr for every item in wanted_mountopts that is not
+ * present in mountopts.  The justwarn boolean toggles between error and
+ * warning message.  Return an error count.
+ */
+int check_mountfsoptions(char *mountopts, char *wanted_mountopts, int justwarn)
+{
+        char *ml, *mlp, *item, *ctx = NULL;
+        int errors = 0;
+
+        if (!(ml = strdup(wanted_mountopts))) {
+                fprintf(stderr, "%s: out of memory\n", progname);
+                exit(1);
+        }
+        mlp = ml;
+        while ((item = strtok_r(mlp, ",", &ctx))) {
+                if (!in_mntlist(item, mountopts)) {
+                        fprintf(stderr, "%s: %s mount option `%s' is missing\n",
+                                progname, justwarn ? "Warning: default"
+                                : "Error: mandatory", item);
+                        errors++;
+                }
+                mlp = NULL;
+        }
+        free(ml);
+        return errors;
+}
+
+/*
+ * Trim embedded white space, leading and trailing commas from string s.
+ */
+void trim_mountfsoptions(char *s)
+{
+        char *p;
+
+        for (p = s; *p; ) {
+                if (isspace(*p)) {
+                        memmove(p, p + 1, strlen(p + 1) + 1);
+                        continue;
+                }
+                p++;
+        }
+
+        while (s[0] == ',')
+                memmove(&s[0], &s[1], strlen(&s[1]) + 1);
+
+        p = s + strlen(s) - 1;
+        while (p >= s && *p == ',')
+                *p-- = '\0';
+}
+
+/* Determine if a device is a block device (as opposed to a file) */
+int is_block(char *devname)
+{
+        struct stat st;
+        int ret = 0;
+
+        ret = access(devname, F_OK);
+        if (ret != 0)
+                return 0;
+        ret = stat(devname, &st);
+        if (ret != 0) {
+                fprintf(stderr, "%s: cannot stat %s\n", progname, devname);
+                return -1;
+        }
+        return S_ISBLK(st.st_mode);
+}
+
+__u64 get_device_size(char *device)
+{
+        int ret, fd;
+        __u64 size = 0;
+
+        fd = open(device, O_RDONLY);
+        if (fd < 0) {
+                fprintf(stderr, "%s: cannot open %s: %s\n",
+                        progname, device, strerror(errno));
+                return 0;
+        }
+
+#ifdef BLKGETSIZE64
+        /* size in bytes. bz5831 */
+        ret = ioctl(fd, BLKGETSIZE64, (void*)&size);
+#else
+        {
+                __u32 lsize = 0;
+                /* size in blocks */
+                ret = ioctl(fd, BLKGETSIZE, (void*)&lsize);
+                size = (__u64)lsize * 512;
+        }
+#endif
+        close(fd);
+        if (ret < 0) {
+                fprintf(stderr, "%s: size ioctl failed: %s\n",
+                        progname, strerror(errno));
+                return 0;
+        }
+
+        vprint("device size = "LPU64"MB\n", size >> 20);
+        /* return value in KB */
+        return size >> 10;
+}
+
+/* Setup a file in the first unused loop_device */
+int loop_setup(struct mkfs_opts *mop)
+{
+        char loop_base[20];
+        char l_device[64];
+        int i, ret = 0;
+
+        /* Figure out the loop device names */
+        if (!access("/dev/loop0", F_OK | R_OK)) {
+                strcpy(loop_base, "/dev/loop\0");
+        } else if (!access("/dev/loop/0", F_OK | R_OK)) {
+                strcpy(loop_base, "/dev/loop/\0");
+        } else {
+                fprintf(stderr, "%s: can't access loop devices\n", progname);
+                return EACCES;
+        }
+
+        /* Find unused loop device */
+        for (i = 0; i < MAX_LOOP_DEVICES; i++) {
+                char cmd[PATH_MAX];
+                int cmdsz = sizeof(cmd);
+
+                sprintf(l_device, "%s%d", loop_base, i);
+                if (access(l_device, F_OK | R_OK))
+                        break;
+                snprintf(cmd, cmdsz, "losetup %s > /dev/null 2>&1", l_device);
+                ret = system(cmd);
+
+                /* losetup gets 1 (ret=256) for non-set-up device */
+                if (ret) {
+                        /* Set up a loopback device to our file */
+                        snprintf(cmd, cmdsz, "losetup %s %s", l_device,
+                                 mop->mo_device);
+                        ret = run_command(cmd, cmdsz);
+                        if (ret == 256)
+                                /* someone else picked up this loop device
+                                 * behind our back */
+                                continue;
+                        if (ret) {
+                                fprintf(stderr, "%s: error %d on losetup: %s\n",
+                                        progname, ret, strerror(ret));
+                                return ret;
+                        }
+                        strscpy(mop->mo_loopdev, l_device,
+                                sizeof(mop->mo_loopdev));
+                        return ret;
+                }
+        }
+
+        fprintf(stderr, "%s: out of loop devices!\n", progname);
+        return EMFILE;
+}
+
+int loop_cleanup(struct mkfs_opts *mop)
+{
+        char cmd[150];
+        int ret = 1;
+        if ((mop->mo_flags & MO_IS_LOOP) && *mop->mo_loopdev) {
+                sprintf(cmd, "losetup -d %s", mop->mo_loopdev);
+                ret = run_command(cmd, sizeof(cmd));
+        }
+        return ret;
+}
+
+int loop_format(struct mkfs_opts *mop)
+{
+        int ret = 0;
+
+        if (mop->mo_device_sz == 0) {
+                fatal();
+                fprintf(stderr, "loop device requires a --device-size= "
+                        "param\n");
+                return EINVAL;
+        }
+
+        ret = creat(mop->mo_device, S_IRUSR|S_IWUSR);
+        if (ret < 0) {
+                ret = errno;
+                fprintf(stderr, "%s: Unable to create backing store: %d\n",
+                        progname, ret);
+        } else {
+                close(ret);
+        }
+
+        ret = truncate(mop->mo_device, mop->mo_device_sz * 1024);
+        if (ret != 0) {
+                ret = errno;
+                fprintf(stderr, "%s: Unable to truncate backing store: %d\n",
+                        progname, ret);
+        }
+
+        return ret;
+}
+
+void osd_print_ldd(char *str, struct lustre_disk_data *ldd)
+{
+        printf("\n   %s:\n", str);
+        printf("Target:     %s\n", ldd->ldd_svname);
+        if (ldd->ldd_flags & LDD_F_NEED_INDEX)
+                printf("Index:      unassigned\n");
+        else
+                printf("Index:      %d\n", ldd->ldd_svindex);
+        if (ldd->ldd_uuid[0])
+                printf("UUID:       %s\n", (char *)ldd->ldd_uuid);
+        printf("Lustre FS:  %s\n", ldd->ldd_fsname);
+        printf("Mount type: %s\n", MT_STR(ldd));
+        printf("Flags:      %#x\n", ldd->ldd_flags);
+        printf("              (%s%s%s%s%s%s%s%s%s%s)\n",
+               IS_MDT(ldd) ? "MDT ":"",
+               IS_OST(ldd) ? "OST ":"",
+               IS_MGS(ldd) ? "MGS ":"",
+               /* should never happen */
+               ldd->ldd_flags & LDD_F_NEED_INDEX ? "needs_index ":"",
+               ldd->ldd_flags & LDD_F_VIRGIN     ? "first_time ":"",
+               ldd->ldd_flags & LDD_F_UPDATE     ? "update ":"",
+               ldd->ldd_flags & LDD_F_WRITECONF  ? "writeconf ":"",
+               ldd->ldd_flags & LDD_F_IAM_DIR  ? "IAM_dir_format ":"",
+               ldd->ldd_flags & LDD_F_NO_PRIMNODE? "no_primnode ":"",
+               /* should never happen */
+               ldd->ldd_flags & LDD_F_UPGRADE14  ? "upgrade1.4 ":"");
+        printf("Persistent mount opts: %s\n", ldd->ldd_mount_opts);
+        /* No longer passed */
+        printf("Parameters:%s\n", ldd->ldd_params);
+        if (ldd->ldd_userdata[0])
+                printf("Comment: %s\n", ldd->ldd_userdata);
+        printf("\n");
+}
+
+/* Write the server config files */
+int osd_write_ldd(struct mkfs_opts *mop)
+{
+        struct lustre_disk_data *ldd = &mop->mo_ldd;
+        int ret;
+
+        switch (ldd->ldd_mount_type) {
+#ifdef HAVE_LDISKFS_OSD
+        case LDD_MT_EXT3:
+        case LDD_MT_LDISKFS:
+        case LDD_MT_LDISKFS2:
+                ret = ldiskfs_write_ldd(mop);
+                break;
+#endif /* HAVE_LDISKFS_OSD */
+        default:
+                fatal();
+                fprintf(stderr, "unknown fs type %d '%s'\n",
+                        ldd->ldd_mount_type, MT_STR(ldd));
+                ret = EINVAL;
+                break;
+        }
+
+        return ret;
+}
+
+/* Read the server config files */
+int osd_read_ldd(char *dev, struct lustre_disk_data *ldd)
+{
+        int ret;
+
+        switch (ldd->ldd_mount_type) {
+#ifdef HAVE_LDISKFS_OSD
+        case LDD_MT_EXT3:
+        case LDD_MT_LDISKFS:
+        case LDD_MT_LDISKFS2:
+                ret = ldiskfs_read_ldd(dev, ldd);
+                break;
+#endif /* HAVE_LDISKFS_OSD */
+        default:
+                fatal();
+                fprintf(stderr, "unknown fs type %d '%s'\n",
+                        ldd->ldd_mount_type, MT_STR(ldd));
+                ret = EINVAL;
+                break;
+        }
+
+        return ret;
+}
+
+/* Was this device formatted for Lustre */
+int osd_is_lustre(char *dev, unsigned *mount_type)
+{
+        vprint("checking for existing Lustre data: ");
+
+#ifdef HAVE_LDISKFS_OSD
+        if (ldiskfs_is_lustre(dev, mount_type)) {
+                vprint("found\n");
+                return 1;
+        }
+#endif /* HAVE_LDISKFS_OSD */
+
+
+        vprint("not found\n");
+        return 0;
+}
+
+/* Build fs according to type */
+int osd_make_lustre(struct mkfs_opts *mop)
+{
+        struct lustre_disk_data *ldd = &mop->mo_ldd;
+        int ret;
+
+        switch (ldd->ldd_mount_type) {
+#ifdef HAVE_LDISKFS_OSD
+        case LDD_MT_EXT3:
+        case LDD_MT_LDISKFS:
+        case LDD_MT_LDISKFS2:
+                ret = ldiskfs_make_lustre(mop);
+                break;
+#endif /* HAVE_LDISKFS_OSD */
+        default:
+                fatal();
+                fprintf(stderr, "unknown fs type %d '%s'\n",
+                        ldd->ldd_mount_type, MT_STR(ldd));
+                ret = EINVAL;
+                break;
+        }
+
+        return ret;
+}
+
+int osd_prepare_lustre(struct mkfs_opts *mop,
+                       char *default_mountopts, int default_len,
+                       char *always_mountopts, int always_len)
+{
+        struct lustre_disk_data *ldd = &mop->mo_ldd;
+        int ret;
+
+        switch (ldd->ldd_mount_type) {
+#ifdef HAVE_LDISKFS_OSD
+        case LDD_MT_EXT3:
+        case LDD_MT_LDISKFS:
+        case LDD_MT_LDISKFS2:
+                ret = ldiskfs_prepare_lustre(mop,
+                                             default_mountopts, default_len,
+                                             always_mountopts, always_len);
+                break;
+#endif /* HAVE_LDISKFS_OSD */
+        default:
+                fatal();
+                fprintf(stderr, "unknown fs type %d '%s'\n",
+                        ldd->ldd_mount_type, MT_STR(ldd));
+                ret = EINVAL;
+                break;
+        }
+
+        return ret;
+}
+
+int osd_tune_lustre(char *dev, struct mount_opts *mop)
+{
+        struct lustre_disk_data *ldd = &mop->mo_ldd;
+        int ret;
+
+        switch (ldd->ldd_mount_type) {
+#ifdef HAVE_LDISKFS_OSD
+        case LDD_MT_EXT3:
+        case LDD_MT_LDISKFS:
+        case LDD_MT_LDISKFS2:
+                ret = ldiskfs_tune_lustre(dev, mop);
+                break;
+#endif /* HAVE_LDISKFS_OSD */
+        default:
+                fatal();
+                fprintf(stderr, "unknown fs type %d '%s'\n",
+                        ldd->ldd_mount_type, MT_STR(ldd));
+                ret = EINVAL;
+                break;
+        }
+
+        return ret;
+}
+
+int osd_init(void)
+{
+        int ret = 0;
+
+#ifdef HAVE_LDISKFS_OSD
+        ret = ldiskfs_init();
+        if (ret)
+                return ret;
+#endif /* HAVE_LDISKFS_OSD */
+
+        return ret;
+}
+
+void osd_fini(void)
+{
+#ifdef HAVE_LDISKFS_OSD
+        ldiskfs_fini();
+#endif /* HAVE_LDISKFS_OSD */
+}
