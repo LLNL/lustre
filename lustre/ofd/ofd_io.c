722b2fa14615102d3fb1c8d543c2f272af8d20dc
Li Wei <liwei@whamcloud.com>
2011-10-19 18:30:40 +0800

    ORI-373 ofd: Fix la_from_obdo() to access only valid obdo fields
    la_from_obdo() ignores oa_valid when accessing obdo fields.  This patch
    fixes the inconsistent behavior to match the logic of iattr_from_obdo()
    on master.  It also cleans up the local variable alignments of
    ofd_commitrw() and ofd_commitrw_write().
    
    Change-Id: I2c0b7ff796b1bc459610165bad4488484d273286
    Signed-off-by: Li Wei <liwei@whamcloud.com>

--- lustre/ofd/ofd_io.c
+++ lustre/ofd/ofd_io.c
@@ -261,13 +261,13 @@ ofd_commitrw_write(const struct lu_env *env, struct ofd_device *ofd,
                       struct obd_trans_info *oti, int old_rc)
 {
         struct ofd_thread_info *info = ofd_info(env);
-        struct ofd_object *fo;
-        struct dt_object     *o;
-        struct lu_attr       *ln = &info->fti_attr2;
-        struct thandle       *th;
-        int                   rc = 0;
-        int retries = 0;
+        struct ofd_object      *fo;
+        struct dt_object       *o;
+        struct lu_attr         *ln = &info->fti_attr2;
+        struct thandle         *th;
         __u64                   valid = 0;
+        int                     rc = 0;
+        int                     retries = 0;
         ENTRY;
 
         LASSERT(objcount == 1);
@@ -410,12 +410,12 @@ int ofd_commitrw(int cmd, struct obd_export *exp,
                     struct niobuf_local *lnb, struct obd_trans_info *oti,
                     int old_rc)
 {
-        struct ofd_device      *ofd = ofd_exp(exp);
-        struct lu_env             *env = oti->oti_env;
         struct ofd_thread_info *info;
         struct ofd_mod_data    *fmd;
         __u64                      valid;
-        int                        rc = 0;
+        struct ofd_device      *ofd = ofd_exp(exp);
+        struct lu_env          *env = oti->oti_env;
+        int                     rc = 0;
 
         info = ofd_info(env);
         ofd_oti2info(info, oti);
