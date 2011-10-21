2712ab1fe05482044522a08b679e1d6870d2d30d
Andreas Dilger <adilger@whamcloud.com>
2011-10-21 04:19:46 -0600

    ORI-145 osd: use "-osd" in OBD device name
    Instead of using "-dsk" in the OBD device names, use "-osd".
    
    Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
    Change-Id: I44846dc7e18bd6362f14c5183d9437b8286f190c
    Reviewed-on: http://review.whamcloud.com/1573
    Tested-by: Hudson

--- lustre/mgs/mgs_llog.c
+++ lustre/mgs/mgs_llog.c
@@ -1411,7 +1411,7 @@ static int mgs_write_log_lod(const struct lu_env *env, struct mgs_device *mgs,
 {
         struct llog_handle *llh = NULL;
         struct lov_desc *lovdesc;
-        char *uuid, *diskname;
+        char *uuid, *osdname;
         int rc = 0;
         ENTRY;
 
@@ -1439,7 +1439,7 @@ static int mgs_write_log_lod(const struct lu_env *env, struct mgs_device *mgs,
         /* can these be the same? */
         uuid = (char *)lovdesc->ld_uuid.uuid;
 
-        rc = name_create(&diskname, logname, "-dsk");
+        rc = name_create(&osdname, logname, "-osd");
         if (rc)
                 GOTO(out_free, rc);
         /* This should always be the first entry in a log.
@@ -1454,7 +1454,7 @@ static int mgs_write_log_lod(const struct lu_env *env, struct mgs_device *mgs,
         rc = record_attach(env, llh, lovname, "lod", uuid);
         if (rc)
                 GOTO(out_end, rc);
-        rc = record_lov_setup(env, llh, lovname, lovdesc, diskname);
+        rc = record_lov_setup(env, llh, lovname, lovdesc, osdname);
         if (rc)
                 GOTO(out_end, rc);
         rc = record_marker(env, llh, fsdb, CM_END, lovname, "lod setup");
@@ -1464,7 +1464,7 @@ static int mgs_write_log_lod(const struct lu_env *env, struct mgs_device *mgs,
 out_end:
         record_end_log(env, &llh);
 out_name:
-        name_destroy(&diskname);
+        name_destroy(&osdname);
 out_free:
         OBD_FREE_PTR(lovdesc);
         return rc;
@@ -2017,7 +2017,7 @@ static int mgs_write_log_osp_to_lod(const struct lu_env *env,
 {
         struct llog_handle *llh = NULL;
         char *nodeuuid, *ospname, *ospuuid, *loduuid, *svname;
-        char *dskname;
+        char *osdname;
         char index[7];
         int i, rc;
 
@@ -2048,7 +2048,7 @@ static int mgs_write_log_osp_to_lod(const struct lu_env *env,
         rc = name_create(&loduuid, lodname, "_UUID");
         if (rc)
                 GOTO(out_ospuuid, rc);
-        rc = name_create(&dskname, logname, "-dsk");
+        rc = name_create(&osdname, logname, "-osd");
         if (rc)
                 GOTO(out_loduuid, rc);
 
@@ -2082,7 +2082,8 @@ static int mgs_write_log_osp_to_lod(const struct lu_env *env,
         rc = record_attach(env, llh, ospname, LUSTRE_OSP_NAME, loduuid);
         if (rc)
                 GOTO(out_end, rc);
-        rc = record_setup(env, llh, ospname, mti->mti_uuid, nodeuuid, dskname, index);
+        rc = record_setup(env, llh, ospname, mti->mti_uuid, nodeuuid,
+                          osdname, index);
         if (rc)
                 GOTO(out_end, rc);
         rc = mgs_write_log_failnids(env, mti, llh, ospname);
@@ -2098,7 +2099,7 @@ static int mgs_write_log_osp_to_lod(const struct lu_env *env,
 out_end:
         record_end_log(env, &llh);
 out_free:
-        name_destroy(&dskname);
+        name_destroy(&osdname);
 out_loduuid:
         name_destroy(&loduuid);
 out_ospuuid:
