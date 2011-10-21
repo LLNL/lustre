2715b36cd17865acaad2e103409d6c394569b5bb
Andreas Dilger <adilger@whamcloud.com>
2011-10-21 02:06:11 -0600

    ORI-382 liblustre: fix missing proc_lustre_root
    Fix liblustre build on the orion branch.
    
    Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
    Change-Id: I5f8b6c7d1b31702f5c6f9dd354640b9b773cdfe7
    Reviewed-on: http://review.whamcloud.com/1569
    Tested-by: Hudson

--- lustre/obdclass/genops.c
+++ lustre/obdclass/genops.c
@@ -188,7 +188,6 @@ int class_register_type(struct obd_ops *dt_ops, struct md_ops *md_ops,
         strcpy(type->typ_name, name);
         cfs_spin_lock_init(&type->obd_type_lock);
 
-#ifdef LPROCFS
         type->typ_procroot = lprocfs_register(type->typ_name, proc_lustre_root,
                                               vars, type);
         if (IS_ERR(type->typ_procroot)) {
@@ -202,7 +201,6 @@ int class_register_type(struct obd_ops *dt_ops, struct md_ops *md_ops,
                         GOTO (failed, rc);
                 }
         }
-#endif
         if (ldt != NULL) {
                 type->typ_lu = ldt;
                 rc = lu_device_type_init(ldt);
