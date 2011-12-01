LLNL/lustre

commit ad4331dcd44867dfd048740253218206244457b6

ORI-339 osd-zfs: Process upcall in zfs ldd funcs
The zfs_read_ldd and zfs_write_ldd functions were not processing the
potentially existent "mdt.identity_upcall=" parameter. This change
implements the needed functionality to correct this behavior.

Signed-off-by: Prakash Surya <surya1@llnl.gov>
Change-Id: I9cb44cb87d4de9ef8d31a5e0998c1c5febbd1b58
Reviewed-on: http://review.whamcloud.com/1504
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: Hudson

lustre/utils/mount_utils_zfs.c
@@ -41,17 +41,18 @@
 #include "mount_utils.h"
 
 /* Persistent mount data is stored in these user  attributes */
-#define LDD_VERSION_PROP        "lustre:version"
-#define LDD_FLAGS_PROP          "lustre:flags"
-#define LDD_INDEX_PROP          "lustre:index"
-#define LDD_FSNAME_PROP         "lustre:fsname"
-#define LDD_SVNAME_PROP         "lustre:svname"
-#define LDD_UUID_PROP           "lustre:uuid"
-#define LDD_USERDATA_PROP       "lustre:userdata"
-#define LDD_MOUNTOPTS_PROP      "lustre:mountopts"
-#define LDD_MGSNODE_PROP        "lustre:mgsnode"
-#define LDD_FAILNODE_PROP       "lustre:failnode"
-#define LDD_FAILMODE_PROP       "lustre:failmode"
+#define LDD_VERSION_PROP                "lustre:version"
+#define LDD_FLAGS_PROP                  "lustre:flags"
+#define LDD_INDEX_PROP                  "lustre:index"
+#define LDD_FSNAME_PROP                 "lustre:fsname"
+#define LDD_SVNAME_PROP                 "lustre:svname"
+#define LDD_UUID_PROP                   "lustre:uuid"
+#define LDD_USERDATA_PROP               "lustre:userdata"
+#define LDD_MOUNTOPTS_PROP              "lustre:mountopts"
+#define LDD_MGSNODE_PROP                "lustre:mgsnode"
+#define LDD_FAILNODE_PROP               "lustre:failnode"
+#define LDD_FAILMODE_PROP               "lustre:failmode"
+#define LDD_IDENTITY_UPCALL_PROP        "lustre:identity_upcall"
 
 static libzfs_handle_t *g_zfs;
 
@@ -155,6 +156,11 @@ int zfs_write_ldd(struct mkfs_opts *mop)
                 goto out_close;
 
         ret = zfs_set_prop_param(zhp, ldd, PARAM_FAILMODE, LDD_FAILMODE_PROP);
+        if (ret)
+                goto out_close;
+
+        ret = zfs_set_prop_param(zhp, ldd, PARAM_MDT PARAM_UPCALL,
+                                 LDD_IDENTITY_UPCALL_PROP);
         if (ret)
                 goto out_close;
 
@@ -279,6 +285,11 @@ int zfs_read_ldd(char *ds,  struct lustre_disk_data *ldd)
                 goto out_close;
 
         ret = zfs_get_prop_param(zhp, ldd, PARAM_FAILMODE, LDD_FAILMODE_PROP);
+        if (ret && (ret != ENOENT))
+                goto out_close;
+
+        ret = zfs_get_prop_param(zhp, ldd, PARAM_MDT PARAM_UPCALL,
+                                 LDD_IDENTITY_UPCALL_PROP);
         if (ret && (ret != ENOENT))
                 goto out_close;
 

commit a9e3efc20402442149b531166120f949c0e44756

ORI-339 osd-zfs: fix build failure due to undefined PARAM_UPCALL
Commit ad4331dcd44867dfd048740253218206244457b6 broke the build
with ZFS OSD because PARAM_UPCALL is not defined. This patch
fixes this problem.

Signed-off-by: Johann Lombardi <johann@whamcloud.com>
Change-Id: I365491694305e62b430390ffe8d816c8f70cd625
Reviewed-on: http://review.whamcloud.com/1760
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>

lustre/include/lustre_param.h
@@ -83,6 +83,7 @@ int class_match_net(char *buf, char *key, __u32 net);
 #define PARAM_FAILMODE             "failover.mode="    /* initial mount only */
 #define PARAM_ACTIVE               "active="           /* activate/deactivate */
 #define PARAM_NETWORK              "network="          /* bind on nid */
+#define PARAM_ID_UPCALL            "identity_upcall="  /* identity upcall */
 
 /* Prefixes for parameters handled by obd's proc methods (XXX_process_config) */
 #define PARAM_OST                  "ost."

lustre/utils/mount_utils_zfs.c
@@ -159,7 +159,7 @@ int zfs_write_ldd(struct mkfs_opts *mop)
         if (ret)
                 goto out_close;
 
-        ret = zfs_set_prop_param(zhp, ldd, PARAM_MDT PARAM_UPCALL,
+        ret = zfs_set_prop_param(zhp, ldd, PARAM_MDT PARAM_ID_UPCALL,
                                  LDD_IDENTITY_UPCALL_PROP);
         if (ret)
                 goto out_close;
@@ -288,7 +288,7 @@ int zfs_read_ldd(char *ds,  struct lustre_disk_data *ldd)
         if (ret && (ret != ENOENT))
                 goto out_close;
 
-        ret = zfs_get_prop_param(zhp, ldd, PARAM_MDT PARAM_UPCALL,
+        ret = zfs_get_prop_param(zhp, ldd, PARAM_MDT PARAM_ID_UPCALL,
                                  LDD_IDENTITY_UPCALL_PROP);
         if (ret && (ret != ENOENT))
                 goto out_close;