21eef0eb07f4e27fc8eaaf6b947dfb4b414256d3
Jinshan Xiong <jinshan.xiong@whamcloud.com>
2011-12-19 16:24:31 -0800

    LU-874 ptlrpc: handle in-flight hqreq correctly
    If there are in-flight requests pending, we shouldn't timeout the
    covering dlm locks; neither should we remove the requests from export
    exp_hp_rpcs list until the requests are handled.
    
    In this patch, the following things are improved:
    1. leave IO rpcs in export's hp list until they are handled;
    2. using interval tree to find rpc overlapped locks;
    3. refresh the lock again after IO rpcs are finished to leave a time
       window for clients to cancel covering dlm locks;
    4. cleanup the code.
    
    Signed-off-by: Jinshan Xiong <jinshan.xiong@whamcloud.com>
    Change-Id: I33e2d113d7929a56389741c06dffb5efb6bf28a3

--- lustre/include/lustre_export.h
+++ lustre/include/lustre_export.h
@@ -219,7 +219,7 @@ struct obd_export {
         cfs_list_t                exp_req_replay_queue;
         /** protects exp_flags and exp_outstanding_replies */
         cfs_spinlock_t            exp_lock;
-        /** protects exp_queued_rpc */
+        /** protects exp_hp_rpcs */
         cfs_spinlock_t            exp_rpc_lock;
         /** Compatibility flags for this export */
         __u64                     exp_connect_flags;
@@ -241,7 +241,7 @@ struct obd_export {
                                   /* client timed out and tried to reconnect,
                                    * but couldn't because of active rpcs */
                                   exp_abort_active_req:1;
-        cfs_list_t                exp_queued_rpc;  /* RPC to be handled */
+        cfs_list_t                exp_hp_rpcs;  /* (potential) HP RPCs */
         /* also protected by exp_lock */
         enum lustre_sec_part      exp_sp_peer;
         struct sptlrpc_flavor     exp_flvr;             /* current */
