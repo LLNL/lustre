7952d971cd18b2be5a148eb766ead49557828105
Jinshan Xiong <jinshan.xiong@whamcloud.com>
2012-05-02 10:23:35 -0700

    LU-1299 clio: a combo patch to fix cl_lock
    In this patch, the following issues are fixed:
    * redefine cl_lock_error(). There are two kinds of error when
      enqueuing a cl_lock: internal errors return from server side and
      local error for example interrupted by signal when waiting for a
      lock. Obviously the latter one is not fatal so we shouldn't err the
      lock out;
    * for fault handling, signal is not allowed;
    * handle return code of ll_fault correctly;
    * redefine ->unuse() to release resource it held if enqueuing lock
      failed;
    * in cl_lock_hold_release(), only CLS_CACHED mode lock will be
    * cached,
      for locks in other states, they will be cancelled voluntarily;
    * other minor fixes.
    
    Signed-off-by: Jinshan Xiong <jinshan.xiong@whamcloud.com>
    Change-Id: Iffce8be356723781b8f33ec9bdc2cf73e9e07138
    Patch-Set: 7

--- lustre/llite/llite_mmap.c
+++ lustre/llite/llite_mmap.c
@@ -215,6 +215,7 @@ struct page *ll_nopage(struct vm_area_struct *vma, unsigned long address,
         unsigned long           ra_flags;
         pgoff_t                 pg_offset;
         int                     result;
+        cfs_sigset_t            set;
         ENTRY;
 
         pg_offset = ((address - vma->vm_start) >> PAGE_SHIFT) + vma->vm_pgoff;
@@ -228,10 +229,19 @@ struct page *ll_nopage(struct vm_area_struct *vma, unsigned long address,
 
         vio = vvp_env_io(env);
         vio->u.fault.ft_vma            = vma;
+        vio->u.fault.ft_vmpage         = NULL;
         vio->u.fault.nopage.ft_address = address;
         vio->u.fault.nopage.ft_type    = type;
 
+        set = cfs_block_allsigs();
         result = cl_io_loop(env, io);
+        cfs_restore_sigs(set);
+
+        page = vio->u.fault.ft_vmpage;
+        if (result != 0 && page != NULL) {
+                page_cache_release(page);
+                page = NOPAGE_SIGBUS;
+        }
 
 out_err:
         if (result == 0)
@@ -248,6 +258,26 @@ out_err:
         RETURN(page);
 }
 #else
+
+static inline int to_fault_error(int result)
+{
+        switch(result) {
+        case 0:
+                result = VM_FAULT_LOCKED;
+                break;
+        case -EFAULT:
+                result = VM_FAULT_NOPAGE;
+                break;
+        case -ENOMEM:
+                result = VM_FAULT_OOM;
+                break;
+        default:
+                result = VM_FAULT_SIGBUS;
+                break;
+        }
+        return result;
+}
+
 /**
  * Lustre implementation of a vm_operations_struct::fault() method, called by
  * VM to server page fault (both in kernel and user space).
@@ -264,6 +294,7 @@ int ll_fault0(struct vm_area_struct *vma, struct vm_fault *vmf)
         struct lu_env           *env;
         struct cl_io            *io;
         struct vvp_io           *vio = NULL;
+        struct page             *vmpage;
         unsigned long            ra_flags;
         struct cl_env_nest       nest;
         int                      result;
@@ -272,29 +303,30 @@ int ll_fault0(struct vm_area_struct *vma, struct vm_fault *vmf)
 
         io = ll_fault_io_init(vma, &env,  &nest, vmf->pgoff, &ra_flags);
         if (IS_ERR(io))
-                RETURN(VM_FAULT_ERROR);
+                RETURN(to_fault_error(PTR_ERR(io)));
 
         result = io->ci_result;
-        if (result < 0)
-                goto out_err;
+        if (result == 0) {
+                vio = vvp_env_io(env);
+                vio->u.fault.ft_vma       = vma;
+                vio->u.fault.ft_vmpage    = NULL;
+                vio->u.fault.fault.ft_vmf = vmf;
 
-        vio = vvp_env_io(env);
-        vio->u.fault.ft_vma       = vma;
-        vio->u.fault.ft_vmpage    = NULL;
-        vio->u.fault.fault.ft_vmf = vmf;
+                result = cl_io_loop(env, io);
 
-        result = cl_io_loop(env, io);
-        fault_ret = vio->u.fault.fault.ft_flags;
-
-out_err:
-        if ((result != 0) && !(fault_ret & VM_FAULT_RETRY))
-                fault_ret |= VM_FAULT_ERROR;
-
-        vma->vm_flags |= ra_flags;
-
+                fault_ret = vio->u.fault.fault.ft_flags;
+                vmpage = vio->u.fault.ft_vmpage;
+                if (result != 0 && vmpage != NULL) {
+                        page_cache_release(vmpage);
+                        vmf->page = NULL;
+                }
+        }
         cl_io_fini(env, io);
         cl_env_nested_put(&nest, env);
 
+        vma->vm_flags |= ra_flags;
+        if (result != 0 && !(fault_ret & VM_FAULT_RETRY))
+                fault_ret |= to_fault_error(result);
         RETURN(fault_ret);
 }
 
@@ -303,6 +335,9 @@ int ll_fault(struct vm_area_struct *vma, struct vm_fault *vmf)
         int count = 0;
         bool printed = false;
         int result;
+        cfs_sigset_t set;
+
+        set = cfs_block_allsigs();
 
 restart:
         result = ll_fault0(vma, vmf);
@@ -329,6 +364,7 @@ restart:
 
                 result |= VM_FAULT_LOCKED;
         }
+        cfs_restore_sigs(set);
         return result;
 }
 #endif
