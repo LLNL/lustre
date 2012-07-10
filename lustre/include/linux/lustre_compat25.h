sdsc/lustre-release

LU-1576 llite: correct page usage count
If kernel has add_to_page_cache_lru(), the ll_pagevec_add() is defined
as an empty function, while page_cache_get(page) only makes sense if
ll_pagevec_add() is defined.

This patch moves page_cache_get into ll_pagevec_add() macro
definition.

Signed-off-by: Bobi Jam <bobijam@whamcloud.com>
Change-Id: Iad98aacff43beec3e7a64fd1a778f549250aa5b8

lustre/include/linux/lustre_compat25.h
@@ -822,7 +822,13 @@ static inline int ll_quota_off(struct super_block *sb, int off, int remount)
 #define ll_add_to_page_cache_lru(pg, mapping, off, gfp) \
         add_to_page_cache(pg, mapping, off, gfp)
 #define ll_pagevec_init(pv, cold)       pagevec_init(&lru_pvec, cold);
-#define ll_pagevec_add(pv, pg)          pagevec_add(pv, pg)
+#define ll_pagevec_add(pv, pg)					\
+	do {							\
+		int __ret;					\
+								\
+		page_cache_get(page);				\
+		__ret = pagevec_add(pv, pg);			\
+	} while (0)
 #define ll_pagevec_lru_add_file(pv)     pagevec_lru_add_file(pv)
 #endif
 

LLNL/lustre

lustre/include/linux/lustre_compat25.h
@@ -842,7 +842,13 @@ static inline int ll_quota_off(struct super_block *sb, int off, int remount)
 #define ll_add_to_page_cache_lru(pg, mapping, off, gfp) \
         add_to_page_cache(pg, mapping, off, gfp)
 #define ll_pagevec_init(pv, cold)       pagevec_init(&lru_pvec, cold);
-#define ll_pagevec_add(pv, pg)          pagevec_add(pv, pg)
+#define ll_pagevec_add(pv, pg)					\
+({								\
+	int __ret;						\
+								\
+	page_cache_get(pg);					\
+	__ret = pagevec_add(pv, pg);				\
+})
 #define ll_pagevec_lru_add_file(pv)     pagevec_lru_add_file(pv)
 #endif
 