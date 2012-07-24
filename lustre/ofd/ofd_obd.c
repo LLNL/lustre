9141cb29b165a31be3de05ee1218265e03b08ce8
Alex Zhuravlev <bzzz@whamcloud.com>
2012-07-24 20:12:20 +0400

    ORI-642 ofd: drop locking in ofd_sync
    to improve concurrency
    
    Signed-off-by: Alex Zhuravlev <bzzz@whamcloud.com>
    Change-Id: Idc7df5d01f745f0356dbf08bf9a151d10b2e5127
    Patch-Set: 2

--- lustre/ofd/ofd_obd.c
+++ lustre/ofd/ofd_obd.c
@@ -1230,23 +1230,25 @@ static int ofd_sync(const struct lu_env *env, struct obd_export *exp,
                 GOTO(out, rc = PTR_ERR(fo));
         }
 
-        ofd_write_lock(env, fo);
         if (!ofd_object_exists(fo))
-                GOTO(unlock, rc = -ENOENT);
+                GOTO(put, rc = -ENOENT);
 
 	if (dt_version_get(env, ofd_object_child(fo)) >
 	    ofd_obd(ofd)->obd_last_committed) {
+		CDEBUG(D_DLMTRACE, "sync "DFID"(%Lu > %Lu)\n",
+		       PFID(lu_object_fid(&fo->ofo_obj.do_lu)),
+		       dt_version_get(env, ofd_object_child(fo)),
+		       ofd_obd(ofd)->obd_last_committed);
 		rc = dt_object_sync(env, ofd_object_child(fo));
 		if (rc)
-			GOTO(unlock, rc);
+			GOTO(put, rc);
 	}
 
         oinfo->oi_oa->o_valid = OBD_MD_FLID;
         rc = ofd_attr_get(env, fo, &info->fti_attr);
         obdo_from_la(oinfo->oi_oa, &info->fti_attr,
                      OFD_VALID_FLAGS);
-unlock:
-        ofd_write_unlock(env, fo);
+put:
         ofd_object_put(env, fo);
 out:
         RETURN(rc);
