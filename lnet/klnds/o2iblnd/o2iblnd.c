LLNL/lustre

commit b3d9d5b930f09f4c2a9e872fa23cdd96d6383ad8

b=14494 Debugging for o2iblnd RDMA timeouts
Add debugging information that will hopefully prove useful when
we hit the "Timed out RDMA with" IB timeouts.

Contributions-by: Isaac Huang <he.huang@sun.com>
Contributions-by: Christopher J. Morrone <morrone2@llnl.gov>

lnet/klnds/o2iblnd/o2iblnd.c
@@ -65,45 +65,6 @@ kiblnd_cksum (void *ptr, int nob)
         return (sum == 0) ? 1 : sum;
 }
 
-static char *
-kiblnd_msgtype2str(int type)
-{
-        switch (type) {
-        case IBLND_MSG_CONNREQ:
-                return "CONNREQ";
-
-        case IBLND_MSG_CONNACK:
-                return "CONNACK";
-
-        case IBLND_MSG_NOOP:
-                return "NOOP";
-
-        case IBLND_MSG_IMMEDIATE:
-                return "IMMEDIATE";
-
-        case IBLND_MSG_PUT_REQ:
-                return "PUT_REQ";
-
-        case IBLND_MSG_PUT_NAK:
-                return "PUT_NAK";
-
-        case IBLND_MSG_PUT_ACK:
-                return "PUT_ACK";
-
-        case IBLND_MSG_PUT_DONE:
-                return "PUT_DONE";
-
-        case IBLND_MSG_GET_REQ:
-                return "GET_REQ";
-
-        case IBLND_MSG_GET_DONE:
-                return "GET_DONE";
-
-        default:
-                return "???";
-        }
-}
-
 static int
 kiblnd_msgtype2size(int type)
 {
@@ -594,12 +555,70 @@ void
 kiblnd_debug_tx (kib_tx_t *tx)
 {
         CDEBUG(D_CONSOLE, "      %p snd %d q %d w %d rc %d dl %lx "
-               "cookie "LPX64" msg %s%s type %x cred %d\n",
+               "cookie "LPX64" msg %s%s type %x cred %d aqt %lx\n",
                tx, tx->tx_sending, tx->tx_queued, tx->tx_waiting,
                tx->tx_status, tx->tx_deadline, tx->tx_cookie,
                tx->tx_lntmsg[0] == NULL ? "-" : "!",
                tx->tx_lntmsg[1] == NULL ? "-" : "!",
-               tx->tx_msg->ibm_type, tx->tx_msg->ibm_credits);
+               tx->tx_msg->ibm_type, tx->tx_msg->ibm_credits,
+               tx->tx_active_q_time);
+}
+
+/* Caller must be holding the reference on conn to prevent the qp from
+   being destroyed while we are using it. */
+void
+kiblnd_debug_ib_qp(kib_conn_t *conn)
+{
+        struct ib_qp_attr attr;
+        int qp_attr_mask = ~0;
+        struct ib_qp_init_attr qp_init_attr;
+
+        if (conn->ibc_cmid == NULL || conn->ibc_cmid->qp == NULL)
+                return;
+
+        ib_query_qp(conn->ibc_cmid->qp, &attr, qp_attr_mask, &qp_init_attr);
+
+        CDEBUG(D_CONSOLE, "   ib_qp: qp_state %d cur_qp_state %d mtu %d mig_state %d\n",
+               attr.qp_state, attr.cur_qp_state, attr.path_mtu,
+               attr.path_mig_state);
+        CDEBUG(D_CONSOLE, "   ib_qp: qkey %u rq_psn %u sq_psn %u dest_qp_num %u\n",
+               attr.qkey, attr.rq_psn, attr.sq_psn, attr.dest_qp_num);
+        CDEBUG(D_CONSOLE, "   ib_qp: qkey %u rq_psn %u sq_psn %u dest_qp_num %u\n",
+               attr.qkey, attr.rq_psn, attr.sq_psn, attr.dest_qp_num);
+        CDEBUG(D_CONSOLE, "   ib_qp_cap: swr %u rwr %u ssge %u rsge %u inline %u\n",
+               attr.cap.max_send_wr, attr.cap.max_recv_wr,
+               attr.cap.max_send_sge, attr.cap.max_recv_sge,
+               attr.cap.max_inline_data);
+        CDEBUG(D_CONSOLE, "   ib_ah_attr     : dlid %hu sl %hu s_p_bits %hx rate %hu flags %hu port %hu\n",
+               attr.ah_attr.dlid,
+               (u16)attr.ah_attr.sl,
+               (u16)attr.ah_attr.src_path_bits,
+               (u16)attr.ah_attr.static_rate,
+               (u16)attr.ah_attr.ah_flags,
+               (u16)attr.ah_attr.port_num);
+        CDEBUG(D_CONSOLE, "   ib_ah_attr(alt): dlid %hu sl %hu s_p_bits %hx rate %hu flags %hu port %hu\n",
+               attr.alt_ah_attr.dlid,
+               (u16)attr.alt_ah_attr.sl,
+               (u16)attr.alt_ah_attr.src_path_bits,
+               (u16)attr.alt_ah_attr.static_rate,
+               (u16)attr.alt_ah_attr.ah_flags,
+               (u16)attr.alt_ah_attr.port_num);
+        CDEBUG(D_CONSOLE, "   ib_qp: pkey %hu alt_pkey %hu en %hu sq %hu\n",
+               attr.pkey_index,
+               attr.alt_pkey_index,
+               (u16)attr.en_sqd_async_notify,
+               (u16)attr.sq_draining);
+        CDEBUG(D_CONSOLE, "   ib_qp: max_rd %hu max_dest %hu min_rnr %hu port %hu\n",
+               (u16)attr.max_rd_atomic,
+               (u16)attr.max_dest_rd_atomic,
+               (u16)attr.min_rnr_timer,
+               (u16)attr.port_num);
+        CDEBUG(D_CONSOLE, "   ib_qp: timeout %hu retry %hu rnr_re %hu alt_port %hu alt_timeout %hu\n",
+               (u16)attr.timeout,
+               (u16)attr.retry_cnt,
+               (u16)attr.rnr_retry,
+               (u16)attr.alt_port_num,
+               (u16)attr.alt_timeout);
 }
 
 void
@@ -617,7 +636,9 @@ kiblnd_debug_conn (kib_conn_t *conn)
                conn->ibc_state, conn->ibc_noops_posted,
                conn->ibc_nsends_posted, conn->ibc_credits,
                conn->ibc_outstanding_credits, conn->ibc_reserved_credits);
-        CDEBUG(D_CONSOLE, "   comms_err %d\n", conn->ibc_comms_error);
+        CDEBUG(D_CONSOLE, "   ready %d scheduled %d comms_err %d last_send %lx\n",
+               conn->ibc_ready, conn->ibc_scheduled, conn->ibc_comms_error,
+               conn->ibc_last_send);
 
         CDEBUG(D_CONSOLE, "   early_rxs:\n");
         cfs_list_for_each(tmp, &conn->ibc_early_rxs)

lnet/klnds/o2iblnd/o2iblnd.h
@@ -123,6 +123,7 @@ typedef struct
 #endif
         int              *kib_require_priv_port;/* accept only privileged ports */
         int              *kib_use_priv_port;    /* use privileged port for active connect */
+        char            **kib_qp_debug_upcall;
 } kib_tunables_t;
 
 extern kib_tunables_t  kiblnd_tunables;
@@ -792,6 +793,45 @@ kiblnd_abort_receives(kib_conn_t *conn)
                      &kiblnd_data.kib_error_qpa, IB_QP_STATE);
 }
 
+static inline char *
+kiblnd_msgtype2str(int type)
+{
+        switch (type) {
+        case IBLND_MSG_CONNREQ:
+                return "CONNREQ";
+
+        case IBLND_MSG_CONNACK:
+                return "CONNACK";
+
+        case IBLND_MSG_NOOP:
+                return "NOOP";
+
+        case IBLND_MSG_IMMEDIATE:
+                return "IMMEDIATE";
+
+        case IBLND_MSG_PUT_REQ:
+                return "PUT_REQ";
+
+        case IBLND_MSG_PUT_NAK:
+                return "PUT_NAK";
+
+        case IBLND_MSG_PUT_ACK:
+                return "PUT_ACK";
+
+        case IBLND_MSG_PUT_DONE:
+                return "PUT_DONE";
+
+        case IBLND_MSG_GET_REQ:
+                return "GET_REQ";
+
+        case IBLND_MSG_GET_DONE:
+                return "GET_DONE";
+
+        default:
+                return "???";
+        }
+}
+
 static inline const char *
 kiblnd_queue2str (kib_conn_t *conn, cfs_list_t *q)
 {
@@ -1012,7 +1052,6 @@ static inline dma_addr_t kiblnd_sg_dma_address(struct ib_device *dev,
 {
         return sg_dma_address(sg);
 }
-
 
 static inline unsigned int kiblnd_sg_dma_len(struct ib_device *dev,
                                              struct scatterlist *sg)

lnet/klnds/o2iblnd/o2iblnd_cb.c
@@ -786,22 +786,30 @@ kiblnd_post_tx_locked (kib_conn_t *conn, kib_tx_t *tx, int credit)
 
         if (conn->ibc_nsends_posted == IBLND_CONCURRENT_SENDS(ver)) {
                 /* tx completions outstanding... */
-                CDEBUG(D_NET, "%s: posted enough\n",
-                       libcfs_nid2str(peer->ibp_nid));
+                CDEBUG(D_NET, "%s[%d%d %d %d %d]: posted enough\n",
+                       libcfs_nid2str(peer->ibp_nid),
+                       conn->ibc_ready, conn->ibc_scheduled,
+                       conn->ibc_credits, conn->ibc_outstanding_credits,
+                       conn->ibc_reserved_credits);
                 return -EAGAIN;
         }
 
         if (credit != 0 && conn->ibc_credits == 0) {   /* no credits */
-                CDEBUG(D_NET, "%s: no credits\n",
-                       libcfs_nid2str(peer->ibp_nid));
+                CDEBUG(D_NET, "%s[%d%d %d %d]: no credits\n",
+                       libcfs_nid2str(peer->ibp_nid),
+                       conn->ibc_ready, conn->ibc_scheduled,
+                       conn->ibc_outstanding_credits,
+                       conn->ibc_reserved_credits);
                 return -EAGAIN;
         }
 
         if (credit != 0 && !IBLND_OOB_CAPABLE(ver) &&
             conn->ibc_credits == 1 &&   /* last credit reserved */
             msg->ibm_type != IBLND_MSG_NOOP) {      /* for NOOP */
-                CDEBUG(D_NET, "%s: not using last credit\n",
-                       libcfs_nid2str(peer->ibp_nid));
+                CDEBUG(D_NET, "%s[%d%d %d]: not using last credit\n",
+                       libcfs_nid2str(peer->ibp_nid),
+                       conn->ibc_ready, conn->ibc_scheduled,
+                       conn->ibc_reserved_credits);
                 return -EAGAIN;
         }
 
@@ -833,6 +841,14 @@ kiblnd_post_tx_locked (kib_conn_t *conn, kib_tx_t *tx, int credit)
         conn->ibc_nsends_posted++;
         if (msg->ibm_type == IBLND_MSG_NOOP)
                 conn->ibc_noops_posted++;
+
+        CDEBUG(D_NET, "%s[%d%d %d %d %d]: tx %s returns %d credits\n",
+               libcfs_nid2str(conn->ibc_peer->ibp_nid),
+               conn->ibc_ready, conn->ibc_scheduled,
+               conn->ibc_credits, conn->ibc_outstanding_credits,
+               conn->ibc_reserved_credits,
+               kiblnd_msgtype2str(tx->tx_msg->ibm_type),
+               tx->tx_msg->ibm_credits);
 
         /* CAVEAT EMPTOR!  This tx could be the PUT_DONE of an RDMA
          * PUT.  If so, it was first queued here as a PUT_REQ, sent and
@@ -3001,16 +3017,44 @@ kiblnd_check_txs_locked(kib_conn_t *conn, cfs_list_t *txs)
 static int
 kiblnd_conn_timed_out_locked(kib_conn_t *conn)
 {
-        return  kiblnd_check_txs_locked(conn, &conn->ibc_tx_queue) ||
-                kiblnd_check_txs_locked(conn, &conn->ibc_tx_noops) ||
-                kiblnd_check_txs_locked(conn, &conn->ibc_tx_queue_rsrvd) ||
-                kiblnd_check_txs_locked(conn, &conn->ibc_tx_queue_nocred) ||
-                kiblnd_check_txs_locked(conn, &conn->ibc_active_txs);
+        void kiblnd_debug_conn(kib_conn_t *);
+        int  cnt = 0;
+
+        if (kiblnd_check_txs_locked(conn, &conn->ibc_tx_queue)) {
+                CERROR("Timed out RDMA on queue ibc_tx_queue"
+                       " (sends that need a credit)\n");
+                cnt++;
+        }
+        if (kiblnd_check_txs_locked(conn, &conn->ibc_tx_noops)) {
+                CERROR("Timed out RDMA on queue ibc_tx_noops");
+                cnt++;
+        }
+        if (kiblnd_check_txs_locked(conn, &conn->ibc_tx_queue_rsrvd)) {
+                CERROR("Timed out RDMA on queue ibc_tx_queue_rsrvd"
+                       " (sends that need to reserve an ACK/DONE msg)\n");
+                cnt++;
+        }
+        if (kiblnd_check_txs_locked(conn, &conn->ibc_tx_queue_nocred)) {
+                CERROR("Timed out RDMA on queue ibc_tx_queue_nocred"
+                       " (sends that don't need a credit)\n");
+                cnt++;
+        }
+        if (kiblnd_check_txs_locked(conn, &conn->ibc_active_txs)) {
+                CERROR("Timed out RDMA on queue ibc_active_txs"
+                       " (active tx awaiting completion)\n");
+                cnt++;
+        }
+
+        if (cnt != 0)
+                kiblnd_debug_conn(conn);
+
+        return cnt;
 }
 
 void
 kiblnd_check_conns (int idx)
 {
+        void kiblnd_debug_ib_qp(kib_conn_t *);
         CFS_LIST_HEAD (closes);
         CFS_LIST_HEAD (checksends);
         cfs_list_t    *peers = &kiblnd_data.kib_peers[idx];
@@ -3054,6 +3098,7 @@ kiblnd_check_conns (int idx)
                                        conn->ibc_credits,
                                        conn->ibc_outstanding_credits,
                                        conn->ibc_reserved_credits);
+                                kiblnd_debug_ib_qp(conn);
                                 cfs_list_add(&conn->ibc_connd_list, &closes);
                         } else {
                                 cfs_list_add(&conn->ibc_connd_list,

lnet/klnds/o2iblnd/o2iblnd_modparams.c
@@ -136,6 +136,10 @@ static int use_privileged_port = 1;
 CFS_MODULE_PARM(use_privileged_port, "i", int, 0644,
                 "use privileged port when initiating connection");
 
+static char *qp_debug_upcall = "";
+CFS_MODULE_PARM(qp_debug_upcall, "s", charp, 0444,
+                "upcall to run when ib timeout occurs");
+
 kib_tunables_t kiblnd_tunables = {
         .kib_dev_failover           = &dev_failover,
         .kib_service                = &service,
@@ -159,7 +163,8 @@ kib_tunables_t kiblnd_tunables = {
         .kib_fmr_cache              = &fmr_cache,
         .kib_pmr_pool_size          = &pmr_pool_size,
         .kib_require_priv_port      = &require_privileged_port,
-        .kib_use_priv_port          = &use_privileged_port
+        .kib_use_priv_port          = &use_privileged_port,
+        .kib_qp_debug_upcall        = &qp_debug_upcall
 };
 
 #if defined(CONFIG_SYSCTL) && !CFS_SYSFS_MODULE_PARM

commit 5128f36869bd016b37b456f90ede26182f62a1bd

ORI-xxx Fix o2iblnd RMDA timeout debug locking
A deadlock in the o2iblnd connection debug code was accidentally
introduced by commit b3d9d5b930f09f4c2a9e872fa23cdd96d6383ad8.
The caller could take the connection lock twice when a timeout,
or other error occured.

To resolve this the debug function has been changed to a _locked
version which will assert if called without the lock held.  At
the moment this is only called from a single place.

Squash this change in to b3d9d5b930f09f4c2a9e872fa23cdd96d6383ad8
when we rebase.  Normally, I would have reverted the previous
commit and applied a new one with the fix.  However, due to
conflicts this was cleaner for now.

Change-Id: Iae7378180765df777dd697a7de3974ced5c1c489
Signed-off-by: Brian Behlendorf <behlendorf1@llnl.gov>
Found-by: Ira Weiny <weiny2@llnl.gov>

lnet/klnds/o2iblnd/o2iblnd.c
@@ -622,12 +622,12 @@ kiblnd_debug_ib_qp(kib_conn_t *conn)
 }
 
 void
-kiblnd_debug_conn (kib_conn_t *conn)
+kiblnd_debug_conn_locked (kib_conn_t *conn)
 {
         cfs_list_t           *tmp;
         int                   i;
 
-        cfs_spin_lock(&conn->ibc_lock);
+	LASSERT(cfs_spin_is_locked(&conn->ibc_lock));
 
         CDEBUG(D_CONSOLE, "conn[%d] %p [version %x] -> %s: \n",
                cfs_atomic_read(&conn->ibc_refcount), conn,
@@ -667,8 +667,6 @@ kiblnd_debug_conn (kib_conn_t *conn)
         CDEBUG(D_CONSOLE, "   rxs:\n");
         for (i = 0; i < IBLND_RX_MSGS(conn->ibc_version); i++)
                 kiblnd_debug_rx(&conn->ibc_rxs[i]);
-
-        cfs_spin_unlock(&conn->ibc_lock);
 }
 
 int

lnet/klnds/o2iblnd/o2iblnd.h
@@ -1103,6 +1103,7 @@ void kiblnd_free_pages (kib_pages_t *p);
 
 int  kiblnd_cm_callback(struct rdma_cm_id *cmid,
                         struct rdma_cm_event *event);
+void kiblnd_debug_conn_locked (kib_conn_t *conn);
 int  kiblnd_translate_mtu(int value);
 
 int  kiblnd_dev_failover(kib_dev_t *dev);

lnet/klnds/o2iblnd/o2iblnd_cb.c
@@ -3025,7 +3025,6 @@ kiblnd_check_txs_locked(kib_conn_t *conn, cfs_list_t *txs)
 static int
 kiblnd_conn_timed_out_locked(kib_conn_t *conn)
 {
-        void kiblnd_debug_conn(kib_conn_t *);
         int  cnt = 0;
 
         if (kiblnd_check_txs_locked(conn, &conn->ibc_tx_queue)) {
@@ -3054,7 +3053,7 @@ kiblnd_conn_timed_out_locked(kib_conn_t *conn)
         }
 
         if (cnt != 0)
-                kiblnd_debug_conn(conn);
+                kiblnd_debug_conn_locked(conn);
 
         return cnt;
 }