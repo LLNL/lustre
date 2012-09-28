c78d7cafea3eebbf2c1904fe23a09d92feb1956c
Christopher J. Morrone <morrone2@llnl.gov>
2012-09-28 16:45:34 -0700

    LU-1095 debug: Standardize mount/umount message
    A first attempt to cleanup some of the gratutious messages which
    are observed when a service mount/unmounts (or fails to).  During
    normal start up a single message that the service was mounted is
    the goal.  On failure a single message describing the failure
    should be enough since the returned error isn't always sufficient
    to clearly determine the problem.
    
    As always LCONSOLE_* messages are prefered for any error we expect
    an administrator to see during normal usage.
    
    Change-Id: I65ba63f40b596e24d0b7cd46b7485a83a10c67be
    Signed-off-by: Brian Behlendorf <behlendorf1@llnl.gov>

--- lustre/ofd/ofd_fs.c
+++ lustre/ofd/ofd_fs.c
@@ -255,7 +255,7 @@ int ofd_groups_init(const struct lu_env *env, struct ofd_device *ofd)
 		}
 	}
 
-	CWARN("%s: %u groups initialized\n",
+	CDEBUG(D_SUPER, "%s: %u groups initialized\n",
 	      ofd_obd(ofd)->obd_name, ofd->ofd_max_group + 1);
 cleanup:
 	RETURN(rc);
@@ -370,7 +370,7 @@ int ofd_server_data_init(const struct lu_env *env, struct ofd_device *ofd)
 		  sizeof(lsd->lsd_padding) == LR_SERVER_SIZE);
 
 	if (last_rcvd_size == 0) {
-		LCONSOLE_WARN("%s: new disk, initializing\n", obd->obd_name);
+		LCONSOLE_INFO("%s: Initializing new disk\n", obd->obd_name);
 
 		memcpy(lsd->lsd_uuid, obd->obd_uuid.uuid,
 		       sizeof(lsd->lsd_uuid));
