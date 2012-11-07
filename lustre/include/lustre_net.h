b9500465111fccc1fa624d17f85c60520e4f5089
Liang Zhen <liang@whamcloud.com>
2012-11-07 12:45:48 +0800

    LU-2263 ptlrpc: decrease ptlrpcd threads number
    By default Lustre creates ptlrpcd for each hyper-thread which is
    too many for fat-core machine, this patch changed the default
    value to CPU core number, and set the upper limit to 32.
    
    Another change in this patch is, ptlrpc_abort_inflight() may release
    imp_lock and reschedule when it's necessary, to avoid one thread
    holding imp_lock for too long.
    
    This patch didn't apply CPU partition APIs to ptlrpcd, that will
    need more efforts.
    
    Signed-off-by: Liang Zhen <liang@whamcloud.com>
    Change-Id: I9c395d086258a66f8853dc432b11d49d0db03659
    Revision-Id: 2

--- lustre/include/lustre_net.h
+++ lustre/include/lustre_net.h
@@ -2274,6 +2274,8 @@ typedef enum {
         PDL_POLICY_PREFERRED    = 4,
 } pdl_policy_t;
 
+#define PTLRPCD_NTHRS_MAX	32
+
 /* ptlrpc/ptlrpcd.c */
 void ptlrpcd_stop(struct ptlrpcd_ctl *pc, int force);
 void ptlrpcd_wake(struct ptlrpc_request *req);
