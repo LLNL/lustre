64c9c469184eb7fe97c94e776019408677299b1c
Prakash Surya <surya1@llnl.gov>
2013-02-11 12:25:23 -0800

    Revert "LU-2683 lov: release all locks in closure to release sublock"
    This reverts commit 727344d11c9c04bef940f81decc6dd0b738f539a.

--- lustre/lov/lovsub_lock.c
+++ lustre/lov/lovsub_lock.c
@@ -107,16 +107,11 @@ static void lovsub_lock_state(const struct lu_env *env,
                 struct lov_lock *lov    = scan->lll_super;
                 struct cl_lock  *parent = lov->lls_cl.cls_lock;
 
-		/*
-		 * if (sub->lss_active != parent) {
-		 *	lovsub_parent_lock(env, lov);
-		 *	cl_lock_signal(env, parent);
-		 *	lovsub_parent_unlock(env, lov);
-		 * }
-		 */
-		/* Used to be the code in the comment, but it's not necessary
-		 * to take parents mutex to just wake them up. */
-		cfs_waitq_broadcast(&parent->cll_wq);
+                if (sub->lss_active != parent) {
+                        lovsub_parent_lock(env, lov);
+                        cl_lock_signal(env, parent);
+                        lovsub_parent_unlock(env, lov);
+                }
         }
         EXIT;
 }
