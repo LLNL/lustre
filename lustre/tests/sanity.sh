f95d0b5753dd9dd38550fa5586373542f19da79b
Li Wei <wei.g.li@intel.com>
2015-05-28 14:29:14 -0700

    LU-6027 mdt: Allow user EAs with empty values
    Setting a user EA with an empty value is a valid case, according to
    attr(5) and some experiments with ext4.  Doing so with Lustre
    currently results in what appears to be a no-op---the EA name won't be
    added or updated (if an EA with the same name already existed).
    
    This patch is back-ported from the following one:
    Lustre-commit: 165f65a7aa07d55d284d419cdaa04eed2d96ade3
    Lustre-change: http://review.whamcloud.com/12900
    
    Test-Parameters: alwaysuploadlogs envdefinitions=ONLY=102r testlist=sanity
    Signed-off-by: Li Wei <wei.g.li@intel.com>
    Change-Id: I45fc2bde65b1210780abb2bae6f84b3065a1a3d4
    Reviewed-on: http://review.whamcloud.com/15033
    Tested-by: Jenkins
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

--- lustre/tests/sanity.sh
+++ lustre/tests/sanity.sh
@@ -6589,6 +6589,21 @@ test_102q() {
 }
 run_test 102q "flistxattr should not return trusted.link EAs for orphans"
 
+test_102r() {
+	local server_version=$(lustre_version_code $SINGLEMDS)
+
+	[[ $server_version -ge $(version_code 2.6.93) ]] ||
+	[[ $server_version -ge $(version_code 2.5.4) &&
+	   $server_version -lt $(version_code 2.5.50) ]] ||
+		{ skip "Need MDS version at least 2.6.93 or 2.5.5"; return; }
+
+	touch $DIR/$tfile || error "touch"
+	setfattr -n user.$(basename $tfile) $DIR/$tfile || error "setfattr"
+	getfattr -n user.$(basename $tfile) $DIR/$tfile || error "getfattr"
+	rm $DIR/$tfile || error "rm"
+}
+run_test 102r "set EAs with empty values"
+
 run_acl_subtest()
 {
     $LUSTRE/tests/acl/run $LUSTRE/tests/acl/$1.test
