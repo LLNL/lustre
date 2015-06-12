003f186454b32a7fb6ba1e7d3f5a9aae29299757
Prakash Surya <surya1@llnl.gov>
2015-06-12 15:29:29 -0700

    LU-2139 ofd: Do async commit if SOFT_SYNC is seen
    This change implements the server side portion of the OBD_BRW_SOFT_SYNC
    agreement. When this flag is seen on an incoming BRW page, the server
    will begin an asynchronous commit operation via dt_commit_async.
    
    The intention is for this operation to commit pages belonging to a
    client which has "too many" outstanding unstable pages in its cache.
    Upon the commit operation completing, the last_commited value should
    percolate out to the client of interest, allowing it to drop some of
    its pinned unstable pages.
    
    Unfortunately, there is currently no optimization to proactively send
    the last_committed value to the client immediately after the commit
    operation succeeds. It must be piggybacked on any future IO or
    pings made by the client.
    
    Added fed_soft_sync_count which is set to a limit of 16 SOFT_SYNC
    RPCs, and it will be reset as soon as there is a non-SOFT_SYNC RPC.
    
    Added ofd_soft_sync_limit as a tunable proc value for the limit of
    fed_soft_sync_count.
    
    This patch is back-ported from the following one:
    Lustre-commit: 9761d5c52aeef31a8c3112c3fb3ec9e24b37c800
    Lustre-change: http://review.whamcloud.com/5935
    
    Signed-off-by: Prakash Surya <surya1@llnl.gov>
    Signed-off-by: Lai Siyao <lai.siyao@intel.com>
    Change-Id: I092625ebeec65e7e69d9563973b4c2c5be18827d

--- lustre/ofd/ofd_internal.h
+++ lustre/ofd/ofd_internal.h
@@ -72,6 +72,8 @@ struct ofd_mod_data {
 #define OFD_FMD_MAX_NUM_DEFAULT 128
 #define OFD_FMD_MAX_AGE_DEFAULT ((obd_timeout + 10) * HZ)
 
+#define OFD_SOFT_SYNC_LIMIT_DEFAULT 16
+
 enum {
 	LPROC_OFD_READ_BYTES = 0,
 	LPROC_OFD_WRITE_BYTES = 1,
@@ -186,6 +188,8 @@ struct ofd_device {
 				  * supporting OBD_CONNECT_GRANT_PARAM? */
 				 ofd_grant_compat_disable:1;
 	struct seq_server_site	 ofd_seq_site;
+	/* the limit of SOFT_SYNC RPCs that will trigger a soft sync */
+	unsigned int		 ofd_soft_sync_limit;
 };
 
 static inline struct ofd_device *ofd_dev(struct lu_device *d)
