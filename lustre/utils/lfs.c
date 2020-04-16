a22ca3a90f2cd50b6f1278eb9a6caee8d52a46e9
Olaf Faaland <faaland1@llnl.gov>
2020-04-15 23:28:44 -0700

    log lfs setstripe paths to syslog
    To help guide PFL implementation, log the path arguments to lfs setstripe when
    issued by users, along with the hostname and slurm JobID.  The information is
    logged to syslog, priority daemon.info.  The syslog message is formatted like:
    
    lfs_arguments jobid=29383 node=pascal23 path="/p/lustre2/faaland1/test.4"
    
    One log message will be emitted for each path specified by an
    'lfs setstripe' command.  The message is logged only if setstripe was
    successful.
    
    Altered man page lfs-setstripe.1 to reflect this.
    
    Signed-off-by: Olaf Faaland <faaland1@llnl.gov>

--- lustre/utils/lfs.c
+++ lustre/utils/lfs.c
@@ -60,6 +60,8 @@
 #include <dirent.h>
 #include <time.h>
 #include <ctype.h>
+#include <syslog.h>
+#include <sys/utsname.h>
 #include "lfs_project.h"
 
 #include <libcfs/util/string.h>
@@ -1015,6 +1017,40 @@ static int parse_targets(__u32 *osts, int size, int offset, char *arg)
 	return rc < 0 ? rc : nr;
 }
 
+#define LFS_PRIORITY		(LOG_DAEMON | LOG_INFO)
+#define LFS_MSG_TAG		"lfs_arguments"
+#define LFS_JOBID_VAR		"SLURM_JOB_ID"
+static void log_to_syslog(const char *in_path)
+{
+	char rp[PATH_MAX];
+	struct utsname uts;
+	int have_uts;
+	char *jobid;
+
+	jobid = getenv(LFS_JOBID_VAR);
+	have_uts = !uname(&uts);
+
+	memset(rp, 0, sizeof(rp));
+
+	/*
+	 * If realpath fails, we need not log the path. log_to_syslog() must be
+	 * called after setstripe has finished
+	 *
+	 * lfs setstripe must be called on a path that either
+	 * (a) refers to a directory, which must exist
+	 * (b) refers to a file, which must not exist. lfs setstripe creates
+	 *     the file.
+	 *
+	 * So after lfs setstripe has finished operating on a path, the path
+	 * refers to a file or directory which exists, if lfs succeeded.
+	 */
+
+	if (realpath(in_path, rp) != NULL)
+		syslog(LFS_PRIORITY, "%s jobid=%s node=%s path=\"%s\"",
+			LFS_MSG_TAG, (jobid ? jobid : "none"),
+			(have_uts ? uts.nodename : "unknown"), rp);
+}
+
 struct lfs_setstripe_args {
 	unsigned long long	 lsa_comp_end;
 	unsigned long long	 lsa_stripe_size;
@@ -1769,6 +1805,8 @@ static int lfs_setstripe(int argc, char **argv)
 				lsa.lsa_pool_name != NULL && result == EINVAL ?
 				"OST not in pool?" : strerror(errno));
 			continue;
+		} else {
+			log_to_syslog(fname);
 		}
 	}
 
