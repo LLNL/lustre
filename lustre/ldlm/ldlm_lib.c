551ea511eea7a2294ad5adb7952f8ab60639f700
Mikhail Pershin <mpershin@whamcloud.com>
2021-08-11 17:30:48 +0300

    LU-14930 mdt: abort_recov_mdt shouldn't abort client recovery
    When abort_recov_mdt is set to abort MDT-MDT recovery then
    abort_recovery flag is set too inside target_stop_recovery_thread()
    call, that causes not just MDT-MDT recovery abort but aborts
    also clients/MDT recovery.
    
    Fixes: dd9e79b64d ("LU-12546 mdt: abort recovery between MDTs")
    Signed-off-by: Mikhail Pershin <mpershin@whamcloud.com>
    Change-Id: Ibda05e91a2da90156e2b6c9fdcb2169cdbd50fe4
    Reviewed-on: https://review.whamcloud.com/44610
    Tested-by: jenkins <devops@whamcloud.com>
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: Alex Zhuravlev <bzzz@whamcloud.com>
    Reviewed-by: Hongchao Zhang <hongchao@whamcloud.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- lustre/ldlm/ldlm_lib.c
+++ lustre/ldlm/ldlm_lib.c
@@ -1827,6 +1827,7 @@ void target_cleanup_recovery(struct obd_device *obd)
 		return;
 	}
 	obd->obd_recovering = obd->obd_abort_recovery = 0;
+	obd->obd_abort_recov_mdt = 0;
 	spin_unlock(&obd->obd_dev_lock);
 
 	spin_lock(&obd->obd_recovery_task_lock);
@@ -2775,6 +2776,7 @@ static int target_recovery_thread(void *arg)
 	 */
 	spin_lock(&obd->obd_dev_lock);
 	obd->obd_recovering = obd->obd_abort_recovery = 0;
+	obd->obd_abort_recov_mdt = 0;
 	spin_unlock(&obd->obd_dev_lock);
 	spin_lock(&obd->obd_recovery_task_lock);
 	target_cancel_recovery_timer(obd);
