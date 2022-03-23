61ee290eeb9c3b4654b7632fae0ed66e5010ec79
Gian-Carlo DeFazio <defazio1@llnl.gov>
2022-03-23 16:25:56 -0700

    LU-14555 lnet: asym route inconsistency warning
    lnet_check_route_inconsistency() checks for inconsistency between
    the lr_hops and lr_single_hop values of a route.
    
    A warning is currently emitted if the route is not single hop
    and the hop count is either 1 or LNET_UNDEFINED_HOPS.
    
    To emit the warning, add the requirement that
    avoid_asym_router_failure is enabled.
    
    Cherry-picked-from: 0b5907952c379f63ba5ba0e658c001ed612fb7dd
    Cherry-picked-from-change: https://review.whamcloud.com/#/c/46918/
    
    Test-Parameters: trivial
    Signed-off-by: Gian-Carlo DeFazio <defazio1@llnl.gov>
    Change-Id: Iaa26d25492e49b569ae5e81da9f00f162be3da59

--- lnet/lnet/router.c
+++ lnet/lnet/router.c
@@ -377,7 +377,9 @@ lnet_consolidate_routes_locked(struct lnet_peer *orig_lp,
 static inline void
 lnet_check_route_inconsistency(struct lnet_route *route)
 {
-	if (!route->lr_single_hop && (int)route->lr_hops <= 1) {
+	if (!route->lr_single_hop &&
+	    (route->lr_hops == 1 || route->lr_hops == LNET_UNDEFINED_HOPS) &&
+	    avoid_asym_router_failure) {
 		CWARN("route %s->%s is detected to be multi-hop but hop count is set to %d\n",
 			libcfs_net2str(route->lr_net),
 			libcfs_nid2str(route->lr_gateway->lp_primary_nid),
