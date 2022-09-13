LLNL/lustre

commit 6105aa036baffe13f07b74e1a78266f3658df900

kfilnd: add kfilnd to specfile
Allow kfilnd to be enabled and disabled in the specfile via with_kfilnd.

Mechanism for finding kfabrics headers is a little inflexible.

lustre.spec.in
@@ -16,6 +16,7 @@
 %bcond_without static
 %bcond_with    systemd
 %bcond_with mofed
+%bcond_with kfi
 %bcond_with mpi
 %bcond_with kabi
 
@@ -466,6 +467,14 @@ fi
 %{?_openmpi_load}
 %{?_mpich_load}
 
+%if %{with kfi}
+	kfi_path=$(ls /usr/src/kfabric-*/default/Module.symvers | sort --sort=version | xargs dirname)
+	if [ -z "$kfi_path" ]; then
+		echo "ERROR: could not find KFabrics devel headers"
+		exit 1
+	fi
+%endif
+
 %if %{with mofed}
 	o2ib_path=$(find /usr/src/ofa_kernel/ -type d -name ofed_scripts | xargs dirname)
 	if [ -z "$o2ib_path" ]; then
@@ -495,6 +504,7 @@ fi
 	%{!?with_systemd:--with-systemdsystemunitdir=no} \
 	%{?with_systemd:--with-systemdsystemunitdir=%{_unitdir}} \
 	%{?with_mofed:--with-o2ib=$o2ib_path} \
+	%{?with_kfi:--with-kfi=$kfi_path} \
 	--with-linux=%{kdir} \
 	--with-linux-obj=%{kobjdir} \
 	--with-kmp-moddir=%{kmoddir}/%{name}

commit f0ea5fa64ae5b19de71b055d6a65ea0499a54448

Put KFILND in a subpackage and build by default
If kfilnd is being built, we need to BuildRequires to ensure we have the
needed headers, and an install Requires for the kfabric kernel modules.

As of this writing, the relevant packages are:
cray-kfabric-devel
kmod-cray-kfabric

fixes: 6105aa0 kfilnd: add kfilnd to specfile

Putting KFILND in a subpackage allows the install time dependency to
apply only if the KFILND is installed so that commodity systems without
slingshot hardware do not need to install the slingshot software they
cannot use.

Build by default so we get it via build farm builds.

rpm/kmp-lustre-lnd-kfi.files
@@ -1,0 +1,2 @@
+%defattr(-,root,root)
+%{modules_fs_path}/%{lustre_name}-lnd-kfi/net/kkfilnd.ko

rpm/kmp-lustre-lnd-kfi.preamble
@@ -1,0 +1,5 @@
+Summary:        Lustre KFILND feature support
+Requires:	kmod-cray-kfabric
+Provides:       %{name}-lnd = %{version}
+Obsoletes:      %{name}-lnd-kfi < %{version}
+BuildRequires:	cray-kfabric-devel

lustre.spec.in
@@ -16,7 +16,7 @@
 %bcond_without static
 %bcond_with    systemd
 %bcond_with mofed
-%bcond_with kfi
+%bcond_without kfi
 %bcond_with mpi
 %bcond_with kabi
 
@@ -186,6 +186,8 @@ Source4: kmp-lustre-osd-ldiskfs.files
 Source5: kmp-lustre-osd-zfs.preamble
 Source6: kmp-lustre-osd-zfs.files
 Source7: kmp-lustre-tests.files
+Source8: kmp-lustre-lnd-kfi.preamble
+Source9: kmp-lustre-lnd-kfi.files
 URL: https://wiki.whamcloud.com/
 BuildRoot: %{_tmppath}/lustre-%{version}-root
 %if %{with lustre_modules}
@@ -289,6 +291,10 @@ LDISKFS hooks for mount/mkfs into a dynamic library.
 # with lustre_utils
 %endif
 # with ldiskfs
+%endif
+
+%if %{with kfi}
+%kernel_module_package -n %{name}-lnd-kfi -p %SOURCE8 -f %SOURCE9 %{_flavor}
 %endif
 
 %if %{with zfs}
@@ -582,6 +588,10 @@ make install DESTDIR=$RPM_BUILD_ROOT
 # Lustre's build system.  This is not expected to bother SLES's
 # kernel_module_path macro.
 basemodpath=$RPM_BUILD_ROOT%{modules_fs_path}/%{name}
+%if %{with kfi}
+mkdir -p $basemodpath-lnd-kfi/net
+mv $basemodpath/net/kkfilnd.ko $basemodpath-lnd-kfi/net/kkfilnd.ko
+%endif
 %if %{with ldiskfs}
 mkdir -p $basemodpath-osd-ldiskfs/fs
 mv $basemodpath/fs/osd_ldiskfs.ko $basemodpath-osd-ldiskfs/fs/osd_ldiskfs.ko