eaf38d0066589ac40a5ed3d9b9a8dcfbcc1ef5a8
Gian-Carlo DeFazio <defazio1@llnl.gov>
2023-04-05 17:20:35 -0700

    TOSS-5943 check ib_post_send failure reason
    ib_post_send may fail due to the invalidate
    work request failing. Mention if the invalidate failed
    in the debug logs.

--- lnet/klnds/o2iblnd/o2iblnd_cb.c
+++ lnet/klnds/o2iblnd/o2iblnd_cb.c
@@ -764,6 +764,7 @@ __must_hold(&conn->ibc_lock)
 	int ver = conn->ibc_version;
 	int rc;
 	int done;
+	int invalidate_succeeded = 0;
 
 	LASSERT(tx->tx_queued);
 	/* We rely on this for QP sizing */
@@ -888,13 +889,15 @@ __must_hold(&conn->ibc_lock)
 #else
 			rc = ib_post_send(conn->ibc_cmid->qp, wr, &bad);
 #endif
+		invalidate_succeeded = bad != &frd->frd_inv_wr.wr;
+
 		if (frd && !frd->frd_posted) {
 			/* The local invalidate becomes invalid (has been
 			 * successfully used) if the post succeeds or the
 			 * failing wr was not the invalidate.
 			 */
 			frd->frd_valid =
-				!(rc == 0 || (bad != &frd->frd_inv_wr.wr));
+				!(rc == 0 || invalidate_succeeded);
 		}
 	}
 
@@ -925,12 +928,14 @@ __must_hold(&conn->ibc_lock)
 
 	spin_unlock(&conn->ibc_lock);
 
-	if (conn->ibc_state == IBLND_CONN_ESTABLISHED)
-		CERROR("posting transmit to %s: rc = %d\n",
-		       libcfs_nidstr(&peer_ni->ibp_nid), rc);
-	else
-		CDEBUG(D_NET, "posting transmit to %s: rc = %d\n",
-		       libcfs_nidstr(&peer_ni->ibp_nid), rc);
+        if (conn->ibc_state == IBLND_CONN_ESTABLISHED)
+                CERROR("Error %d posting transmit to %s%s\n",
+		       rc, libcfs_nidstr(&peer_ni->ibp_nid),
+		       invalidate_succeeded ? "" : " ,invalidate failed");
+        else
+                CDEBUG(D_NET, "Error %d posting transmit to %s%s\n",
+		       rc, libcfs_nidstr(&peer_ni->ibp_nid),
+		       invalidate_succeeded ? "" : " ,invalidate failed");
 
 	kiblnd_close_conn(conn, rc);
 
