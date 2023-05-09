c76c54668b7d149da85cd29bdad95f768da595ba
Timothy Day <timday@amazon.com>
2023-05-09 05:07:36 +0000

    LU-8802 obd: remove MAX_OBD_DEVICES
    Remove this arbitrary limit by reimplementing the array as an
    Xarray. Xarray can grow and shink dynamically, hence saving
    memory and allow for many more OBD devices. There is still
    technically a limit OBD_MAX_INDEX, which is xa_limit_31b.max
    or around 2 billion. This is far more than is practically
    useful.
    
    This patch also adds various iterators for OBD devices, which
    are used to simplify code in various places.
    
    Removing class_obd_list() since it is unused. Rename
    class_dev_by_str() to class_str2obd() to keep the pattern.
    Several class_* functions have been refactored to improve
    locking. The larger issue of OBD device locking will be
    addressed separately.
    
    Update the OBD device lifecycle test to try loading
    more devices (about 24,000 for now).
    
    Currently, adding an additional OBD device is an O(n^2)
    operation due to the class_name2dev calls in
    class_register_device(). This will be addressed in a future
    patch adding a hash table for OBD device name lookups.
    
    Further, OBD life cycle management could likely be simplified
    by using Xarray marks. Right now, it is handled by a bit
    field in the obd_device struct. Since the scope of the changes
    needed to simplify this seem large, this will also be addressed
    separately.
    
    Cherry-picked-from: c5e5060d9502c2e4fb43579d02c983ccbec9b063
    Cherry-picked-from-change: https://review.whamcloud.com/51040
    
    Test-Parameters: testlist=sanity env=ONLY=55,ONLY_REPEAT=10
    Signed-off-by: Timothy Day <timday@amazon.com>
    Change-Id: Icb2cd94a5529e79f5d3ebd0de5e0f225cf212075
    Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/51040
    Tested-by: jenkins <devops@whamcloud.com>
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: James Simmons <jsimmons@infradead.org>
    Reviewed-by: Neil Brown <neilb@suse.de>
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>
    Signed-off-by: Gian-Carlo DeFazio <defazio1@llnl.gov>

--- lustre/llite/llite_lib.c
+++ lustre/llite/llite_lib.c
@@ -1453,10 +1453,12 @@ void ll_put_super(struct super_block *sb)
 	struct obd_device *obd;
 	struct lustre_sb_info *lsi = s2lsi(sb);
 	struct ll_sb_info *sbi = ll_s2sbi(sb);
+	unsigned long dev_no = 0;
 	char *profilenm = get_profile_name(sb);
 	unsigned long cfg_instance = ll_get_cfg_instance(sb);
 	long ccc_count;
-	int next, force = 1, rc = 0;
+	int force = 1, rc = 0;
+
 	ENTRY;
 
 	if (IS_ERR(sbi))
@@ -1493,11 +1495,10 @@ void ll_put_super(struct super_block *sb)
 	 * lustre_common_put_super, since l_d cleans up osc's as well.
 	 */
 	if (force) {
-		next = 0;
-		while ((obd = class_devices_in_group(&sbi->ll_sb_uuid,
-						     &next)) != NULL) {
+		obd_device_lock();
+		obd_device_for_each_uuid(dev_no, obd, &sbi->ll_sb_uuid)
 			obd->obd_force = force;
-		}
+		obd_device_unlock();
 	}
 
 	if (sbi->ll_client_common_fill_super_succeeded) {
@@ -1505,8 +1506,12 @@ void ll_put_super(struct super_block *sb)
 		client_common_put_super(sb);
 	}
 
-	next = 0;
-	while ((obd = class_devices_in_group(&sbi->ll_sb_uuid, &next)))
+	/*
+	 * Cleanup, detach OBD devices, and remove them from Xarray.
+	 * We don't grab the xa_lock() since class_manual_cleanup()
+	 * uses the lock internally.
+	 */
+	obd_device_for_each_uuid(dev_no, obd, &sbi->ll_sb_uuid)
 		class_manual_cleanup(obd);
 
 	if (test_bit(LL_SBI_VERBOSE, sbi->ll_flags))
