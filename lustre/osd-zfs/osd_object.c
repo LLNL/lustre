03376d8f02bb18fcb9251a7358005c30776154c4
Shaun Tancheff <shaun.tancheff@hpe.com>
2023-05-22 07:36:21 -0500

    LU-16053 build: Update zfs configure checks
    From Brian Behlendorf <behlendorf1@llnl.gov>:
    
    update dmu_*_by_dnode checks
     Provided as a feature since ZFS 0.7.0, convert to a fatal configure
     error when unavailable.
    
    update zap_*_by_dnode checks
     Provided as a feature since ZFS 0.7.0, convert to a fatal configure
     error when unavailable.
    
    update multihost protection check
     Provided as a feature since ZFS 0.7.0, convert to a fatal configure
     error when unavailable.  Drop the compatibility code required to
     support OpenZFS releases older than 0.7.0.
    
    update userobj accounting check
     Provided as a feature since ZFS 0.7.0, convert to a fatal configure
     error when unavailable.
    
    update dmu_prefetch() check
     Provided since at least ZFS 0.7.0, convert to a fatal configure
     error when unavailable.
    
    update dmu_object_alloc_dnsize() check
     Provided since at least ZFS 0.7.0, convert to a fatal configure
     error when unavailable.
    
    update spa_maxblocksize() check
     Provided since at least ZFS 0.7.0, convert to a fatal configure
     error when unavailable.
    
    update dsl_pool_config_enter/exit check
     Convert to a fatal configure error, these functions have
     been provided since at least ZFS 0.7.x.
    
    replace sa_spill_block() check
     The sa_spill_block() function was removed after the ZFS 0.6.x
     release.  Replace the check with one for use zio_buf_alloc/free
     which have been available since 0.7.x.
    
     The dsl_sync_task_do_nowait() function has not been provided
     by since the 0.6.x releases.  Furthermore, the results of this
     check are unused by Lustre so let's just remove it.
    
    Test-Parameters: trivial
    Signed-off-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
    Change-Id: I3c1597e56100961178f9001e918ffb9aa3558706
    Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/48089
    Tested-by: jenkins <devops@whamcloud.com>
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
    Reviewed-by: James Simmons <jsimmons@infradead.org>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- lustre/osd-zfs/osd_object.c
+++ lustre/osd-zfs/osd_object.c
@@ -1034,7 +1034,7 @@ static int osd_attr_get(const struct lu_env *env, struct dt_object *dt,
 	}
 	/* Block size may be not set; suggest maximal I/O transfers. */
 	if (blksize == 0)
-		blksize = osd_spa_maxblocksize(
+		blksize = spa_maxblocksize(
 			dmu_objset_spa(osd_obj2dev(obj)->od_os));
 
 	attr->la_blksize = blksize;
@@ -1611,7 +1611,7 @@ int __osd_attr_init(const struct lu_env *env, struct osd_device *osd,
 		rc = -nvlist_size(xattr, &sa_size, NV_ENCODE_XDR);
 		LASSERT(rc == 0);
 
-		dxattr = osd_zio_buf_alloc(sa_size);
+		dxattr = zio_buf_alloc(sa_size);
 		LASSERT(dxattr);
 
 		rc = -nvlist_pack(xattr, &dxattr, &sa_size,
@@ -1624,7 +1624,7 @@ int __osd_attr_init(const struct lu_env *env, struct osd_device *osd,
 
 	rc = -sa_replace_all_by_template(sa_hdl, bulk, cnt, tx);
 	if (dxattr)
-		osd_zio_buf_free(dxattr, sa_size);
+		zio_buf_free(dxattr, sa_size);
 
 	return rc;
 }
@@ -1672,7 +1672,6 @@ int osd_find_new_dnode(const struct lu_env *env, dmu_tx_t *tx,
 	return rc;
 }
 
-#ifdef HAVE_DMU_OBJECT_ALLOC_DNSIZE
 int osd_find_dnsize(struct osd_device *osd, int ea_in_bonus)
 {
 	int dnsize;
@@ -1701,7 +1700,6 @@ int osd_find_dnsize(struct osd_device *osd, int ea_in_bonus)
 	}
 	return dnsize;
 }
-#endif
 
 /*
  * The transaction passed to this routine must have
