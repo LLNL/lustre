lustre/lustre-release

commit f05edf8e2b92e2c018aad130cd7a1dcc00a8eeee

LU-13791 sec: enable FS capabilities
FS capabilities are not effective because they are dropped for
non-root users for historical reason: they are used to be enforced
before operations, but now they are checked in MDD layer only (see
mdd_fix_attr()).

Add sanity-sec 51.

Signed-off-by: Lai Siyao <lai.siyao@whamcloud.com>
Change-Id: I3e355f5df5eab5509b5e6774dbc8b82281a34039
Reviewed-on: https://review.whamcloud.com/39399
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>
Reviewed-by: Emoly Liu <emoly@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/mdt/mdt_internal.h
@@ -933,7 +933,6 @@ void mdt_dump_lmv(unsigned int level, const union lmv_mds_md *lmv);
 bool allow_client_chgrp(struct mdt_thread_info *info, struct lu_ucred *uc);
 int mdt_check_ucred(struct mdt_thread_info *);
 int mdt_init_ucred(struct mdt_thread_info *, struct mdt_body *);
-int mdt_init_ucred_intent_getattr(struct mdt_thread_info *, struct mdt_body *);
 int mdt_init_ucred_reint(struct mdt_thread_info *);
 void mdt_exit_ucred(struct mdt_thread_info *);
 int mdt_version_get_check(struct mdt_thread_info *, struct mdt_object *, int);

lustre/mdt/mdt_lib.c
@@ -166,7 +166,7 @@ static void ucred_set_audit_enabled(struct mdt_thread_info *info,
 }
 
 static int new_init_ucred(struct mdt_thread_info *info, ucred_init_type_t type,
-			  void *buf, bool drop_fs_cap)
+			  void *buf)
 {
 	struct ptlrpc_request *req = mdt_info_req(info);
 	struct mdt_device *mdt = info->mti_mdt;
@@ -324,11 +324,7 @@ static int new_init_ucred(struct mdt_thread_info *info, ucred_init_type_t type,
 	/* process root_squash here. */
 	mdt_root_squash(info, peernid);
 
-	/* remove fs privilege for non-root user. */
-	if (ucred->uc_fsuid && drop_fs_cap)
-		ucred->uc_cap = pud->pud_cap & ~CFS_CAP_FS_MASK;
-	else
-		ucred->uc_cap = pud->pud_cap;
+	ucred->uc_cap = pud->pud_cap;
 	ucred->uc_valid = UCRED_NEW;
 	ucred_set_jobid(info, ucred);
 	ucred_set_nid(info, ucred);
@@ -462,8 +458,7 @@ out:
 }
 
 static int old_init_ucred_common(struct mdt_thread_info *info,
-				 struct lu_nodemap *nodemap,
-				 bool drop_fs_cap)
+				 struct lu_nodemap *nodemap)
 {
 	struct lu_ucred		*uc = mdt_ucred(info);
 	struct mdt_device	*mdt = info->mti_mdt;
@@ -500,9 +495,6 @@ static int old_init_ucred_common(struct mdt_thread_info *info,
 	/* process root_squash here. */
 	mdt_root_squash(info, mdt_info_req(info)->rq_peer.nid);
 
-	/* remove fs privilege for non-root user. */
-	if (uc->uc_fsuid && drop_fs_cap)
-		uc->uc_cap &= ~CFS_CAP_FS_MASK;
 	uc->uc_valid = UCRED_OLD;
 	ucred_set_jobid(info, uc);
 	ucred_set_nid(info, uc);
@@ -514,7 +506,7 @@ static int old_init_ucred_common(struct mdt_thread_info *info,
 }
 
 static int old_init_ucred(struct mdt_thread_info *info,
-			  struct mdt_body *body, bool drop_fs_cap)
+			  struct mdt_body *body)
 {
 	struct lu_ucred *uc = mdt_ucred(info);
 	struct lu_nodemap *nodemap;
@@ -545,7 +537,7 @@ static int old_init_ucred(struct mdt_thread_info *info,
 	uc->uc_ginfo = NULL;
 	uc->uc_cap = body->mbo_capability;
 
-	rc = old_init_ucred_common(info, nodemap, drop_fs_cap);
+	rc = old_init_ucred_common(info, nodemap);
 	nodemap_putref(nodemap);
 
 	RETURN(rc);
@@ -574,15 +566,14 @@ static int old_init_ucred_reint(struct mdt_thread_info *info)
 	uc->uc_o_gid = uc->uc_o_fsgid = uc->uc_gid = uc->uc_fsgid;
 	uc->uc_ginfo = NULL;
 
-	rc = old_init_ucred_common(info, nodemap, true); /* drop_fs_cap=true */
+	rc = old_init_ucred_common(info, nodemap);
 	nodemap_putref(nodemap);
 
 	RETURN(rc);
 }
 
 static inline int __mdt_init_ucred(struct mdt_thread_info *info,
-				   struct mdt_body *body,
-				   bool drop_fs_cap)
+				   struct mdt_body *body)
 {
 	struct ptlrpc_request	*req = mdt_info_req(info);
 	struct lu_ucred		*uc  = mdt_ucred(info);
@@ -594,25 +585,14 @@ static inline int __mdt_init_ucred(struct mdt_thread_info *info,
 	mdt_exit_ucred(info);
 
 	if (!req->rq_auth_gss || req->rq_auth_usr_mdt || !req->rq_user_desc)
-		return old_init_ucred(info, body, drop_fs_cap);
+		return old_init_ucred(info, body);
 	else
-		return new_init_ucred(info, BODY_INIT, body, drop_fs_cap);
+		return new_init_ucred(info, BODY_INIT, body);
 }
 
 int mdt_init_ucred(struct mdt_thread_info *info, struct mdt_body *body)
 {
-	return __mdt_init_ucred(info, body, true);
-}
-
-/* LU-6528 when "no_subtree_check" is set for NFS export, nfsd_set_fh_dentry()
- * doesn't set correct fsuid explicitely, but raise capability to allow
- * exportfs_decode_fh() to reconnect disconnected dentry into dcache. So for
- * lookup (i.e. intent_getattr), we should keep FS capability, otherwise it
- * will fail permission check. */
-int mdt_init_ucred_intent_getattr(struct mdt_thread_info *info,
-				  struct mdt_body *body)
-{
-	return __mdt_init_ucred(info, body, false);
+	return __mdt_init_ucred(info, body);
 }
 
 int mdt_init_ucred_reint(struct mdt_thread_info *info)
@@ -635,7 +615,7 @@ int mdt_init_ucred_reint(struct mdt_thread_info *info)
 	if (!req->rq_auth_gss || req->rq_auth_usr_mdt || !req->rq_user_desc)
 		return old_init_ucred_reint(info);
 	else
-		return new_init_ucred(info, REC_INIT, NULL, true);
+		return new_init_ucred(info, REC_INIT, NULL);
 }
 
 /* copied from lov/lov_ea.c, just for debugging, will be removed later */

lustre/tests/sanity-sec.sh
@@ -3725,6 +3725,45 @@ test_50() {
 }
 run_test 50 "DoM encrypted file"
 
+test_51() {
+	[ "$MDS1_VERSION" -gt $(version_code 2.13.53) ] ||
+		skip "Need MDS version at least 2.13.53"
+
+	mkdir $DIR/$tdir || error "mkdir $tdir"
+
+	touch $DIR/$tdir/$tfile || error "touch $tfile"
+	cp $(which chown) $DIR/$tdir || error "cp chown"
+	$RUNAS_CMD -u $ID0 $DIR/$tdir/chown $ID0 $DIR/$tdir/$tfile &&
+		error "chown $tfile should fail"
+	setcap 'CAP_CHOWN=ep' $DIR/$tdir/chown || error "setcap CAP_CHOWN"
+	$RUNAS_CMD -u $ID0 $DIR/$tdir/chown $ID0 $DIR/$tdir/$tfile ||
+		error "chown $tfile"
+	rm $DIR/$tdir/$tfile || error "rm $tfile"
+
+	touch $DIR/$tdir/$tfile || error "touch $tfile"
+	cp $(which touch) $DIR/$tdir || error "cp touch"
+	$RUNAS_CMD -u $ID0 $DIR/$tdir/touch $DIR/$tdir/$tfile &&
+		error "touch should fail"
+	setcap 'CAP_FOWNER=ep' $DIR/$tdir/touch || error "setcap CAP_FOWNER"
+	$RUNAS_CMD -u $ID0 $DIR/$tdir/touch $DIR/$tdir/$tfile ||
+		error "touch $tfile"
+	rm $DIR/$tdir/$tfile || error "rm $tfile"
+
+	local cap
+	for cap in "CAP_DAC_OVERRIDE" "CAP_DAC_READ_SEARCH"; do
+		touch $DIR/$tdir/$tfile || error "touch $tfile"
+		chmod 600 $DIR/$tdir/$tfile || error "chmod $tfile"
+		cp $(which cat) $DIR/$tdir || error "cp cat"
+		$RUNAS_CMD -u $ID0 $DIR/$tdir/cat $DIR/$tdir/$tfile &&
+			error "cat should fail"
+		setcap $cap=ep $DIR/$tdir/cat || error "setcap $cap"
+		$RUNAS_CMD -u $ID0 $DIR/$tdir/cat $DIR/$tdir/$tfile ||
+			error "cat $tfile"
+		rm $DIR/$tdir/$tfile || error "rm $tfile"
+	done
+}
+run_test 51 "FS capabilities ==============="
+
 log "cleanup: ======================================================"
 
 sec_unsetup() {

lustre/mdt/mdt_handler.c
@@ -2162,37 +2162,38 @@ unlock_parent:
 static int mdt_getattr_name(struct tgt_session_info *tsi)
 {
 	struct mdt_thread_info	*info = tsi2mdt_info(tsi);
-        struct mdt_lock_handle *lhc = &info->mti_lh[MDT_LH_CHILD];
-        struct mdt_body        *reqbody;
-        struct mdt_body        *repbody;
-        int rc, rc2;
-        ENTRY;
+	struct mdt_lock_handle *lhc = &info->mti_lh[MDT_LH_CHILD];
+	struct mdt_body	*reqbody;
+	struct mdt_body	*repbody;
+	int rc, rc2;
 
-        reqbody = req_capsule_client_get(info->mti_pill, &RMF_MDT_BODY);
-        LASSERT(reqbody != NULL);
-        repbody = req_capsule_server_get(info->mti_pill, &RMF_MDT_BODY);
-        LASSERT(repbody != NULL);
+	ENTRY;
+
+	reqbody = req_capsule_client_get(info->mti_pill, &RMF_MDT_BODY);
+	LASSERT(reqbody != NULL);
+	repbody = req_capsule_server_get(info->mti_pill, &RMF_MDT_BODY);
+	LASSERT(repbody != NULL);
 
 	info->mti_cross_ref = !!(reqbody->mbo_valid & OBD_MD_FLCROSSREF);
 	repbody->mbo_eadatasize = 0;
 	repbody->mbo_aclsize = 0;
 
-        rc = mdt_init_ucred_intent_getattr(info, reqbody);
-        if (unlikely(rc))
-                GOTO(out_shrink, rc);
+	rc = mdt_init_ucred(info, reqbody);
+	if (unlikely(rc))
+		GOTO(out_shrink, rc);
 
-        rc = mdt_getattr_name_lock(info, lhc, MDS_INODELOCK_UPDATE, NULL);
-        if (lustre_handle_is_used(&lhc->mlh_reg_lh)) {
-                ldlm_lock_decref(&lhc->mlh_reg_lh, lhc->mlh_reg_mode);
-                lhc->mlh_reg_lh.cookie = 0;
-        }
-        mdt_exit_ucred(info);
-        EXIT;
+	rc = mdt_getattr_name_lock(info, lhc, MDS_INODELOCK_UPDATE, NULL);
+	if (lustre_handle_is_used(&lhc->mlh_reg_lh)) {
+		ldlm_lock_decref(&lhc->mlh_reg_lh, lhc->mlh_reg_mode);
+		lhc->mlh_reg_lh.cookie = 0;
+	}
+	mdt_exit_ucred(info);
+	EXIT;
 out_shrink:
-        mdt_client_compatibility(info);
-        rc2 = mdt_fix_reply(info);
-        if (rc == 0)
-                rc = rc2;
+	mdt_client_compatibility(info);
+	rc2 = mdt_fix_reply(info);
+	if (rc == 0)
+		rc = rc2;
 	mdt_thread_info_fini(info);
 	return rc;
 }
@@ -4259,7 +4260,7 @@ static int mdt_intent_getattr(enum ldlm_intent_flags it_opc,
 		GOTO(out_shrink, rc = -EINVAL);
 	}
 
-	rc = mdt_init_ucred_intent_getattr(info, reqbody);
+	rc = mdt_init_ucred(info, reqbody);
 	if (rc)
 		GOTO(out_shrink, rc);
 

LLNL/lustre

commit 5a7a2b24fa07a52679f92be849fd134ee0151c69

LU-13791 mdt: parameter to tune capabilities
Add mdt.*.enable_cap_mask to allow specific capabilities to
be enabled and disabled individually.

Too enable specific capabilities to be usable by non-root users
on the client, set the appropriate bits in the mask.  The individual
CAP_* capabilities are defined in /usr/include/linux/capability.h
and read/written in hexadecimal format.

For example, the CAP_DAC_READ_SEARCH capability to allow processes
to read and search all files in the directory tree (e.g. for a
backup utility) is defined as bit #2.  To permanently allow this
capability to be used (with appropriate permissions *as checked by
the client*) for all client nodes, set the value (1<<2) = 4 by
running the following command on the MGS node as root:

    lctl set_param -P mdt.FSNAME-*.enable_cap_mask=0x4

An improved interface for setting the capabilities will be available
in a future release.

Fixes: f05edf8e2b ("LU-13791 sec: enable FS capabilities")
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Change-Id: I6fc0130a90693d673d8c2158e7e31c2de951553d

lustre/mdt/mdt_internal.h
@@ -300,6 +300,7 @@ struct mdt_device {
 	gid_t			   mdt_enable_remote_dir_gid;
 				   /* user with this gid can change projid */
 	gid_t			   mdt_enable_chprojid_gid;
+	kernel_cap_t		   mdt_enable_cap_mask;
 
 	/* lock for osfs and md_root */
 	spinlock_t		   mdt_lock;

lustre/mdt/mdt_lib.c
@@ -326,6 +326,21 @@ static int new_init_ucred(struct mdt_thread_info *info, ucred_init_type_t type,
 	/* process root_squash here. */
 	mdt_root_squash(info, peernid);
 
+	if (ucred->uc_fsuid) {
+		if (!cap_isclear(ucred->uc_cap))
+			CDEBUG(D_SEC, "%s: drop capabilities %llx for NID %s\n",
+			       mdt_obd_name(mdt),
+#ifdef CAP_FOR_EACH_U32
+			       ucred->uc_cap.cap[0] |
+			       ((u64)ucred->uc_cap.cap[1] << 32),
+#else
+			       ucred->uc_cap.val,
+#endif
+			       libcfs_nid2str(mdt_info_req(info)->rq_peer.nid));
+		ucred->uc_cap = cap_intersect(ucred->uc_cap,
+					      mdt->mdt_enable_cap_mask);
+	}
+
 	ucred->uc_valid = UCRED_NEW;
 	ucred_set_jobid(info, ucred);
 	ucred_set_nid(info, ucred);
@@ -495,6 +510,18 @@ static int old_init_ucred_common(struct mdt_thread_info *info,
 	/* process root_squash here. */
 	mdt_root_squash(info, mdt_info_req(info)->rq_peer.nid);
 
+	if (uc->uc_fsuid) {
+		if (!cap_isclear(uc->uc_cap))
+			CDEBUG(D_SEC, "%s: drop capabilities %llx for NID %s\n",
+			       mdt_obd_name(mdt),
+#ifdef CAP_FOR_EACH_U32
+			       uc->uc_cap.cap[0] | ((u64)uc->uc_cap.cap[1]<<32),
+#else
+			       uc->uc_cap.val,
+#endif
+			       libcfs_nid2str(mdt_info_req(info)->rq_peer.nid));
+		uc->uc_cap = cap_intersect(uc->uc_cap,mdt->mdt_enable_cap_mask);
+	}
 	uc->uc_valid = UCRED_OLD;
 	ucred_set_jobid(info, uc);
 	ucred_set_nid(info, uc);

lustre/mdt/mdt_lproc.c
@@ -644,6 +644,55 @@ static ssize_t enable_remote_dir_store(struct kobject *kobj,
 }
 LUSTRE_RW_ATTR(enable_remote_dir);
 
+static ssize_t enable_cap_mask_show(struct kobject *kobj,
+				    struct attribute *attr, char *buf)
+{
+	struct obd_device *obd = container_of(kobj, struct obd_device,
+					      obd_kset.kobj);
+	struct mdt_device *mdt = mdt_dev(obd->obd_lu_dev);
+	u64 cap;
+
+	BUILD_BUG_ON(_KERNEL_CAP_T_SIZE != sizeof(u64));
+
+#ifdef CAP_FOR_EACH_U32 /* kernels before v6.2-13111-gf122a08b197d */
+	cap = ((u64)mdt->mdt_enable_cap_mask.cap[1] << 32) |
+	       mdt->mdt_enable_cap_mask.cap[0];
+#else
+	cap = mdt->mdt_enable_cap_mask.val;
+#endif
+	return scnprintf(buf, PAGE_SIZE, "%#0llx\n", cap);
+}
+
+static ssize_t enable_cap_mask_store(struct kobject *kobj,
+				     struct attribute *attr,
+				     const char *buffer, size_t count)
+{
+	struct obd_device *obd = container_of(kobj, struct obd_device,
+					      obd_kset.kobj);
+	struct mdt_device *mdt = mdt_dev(obd->obd_lu_dev);
+	unsigned long long val;
+	int rc;
+
+	rc = kstrtoull(buffer, 0, &val);
+	if (rc)
+		/* should also accept symbolic names via cfs_str2mask() */
+		return rc;
+
+#ifdef CAP_FOR_EACH_U32
+	mdt->mdt_enable_cap_mask.cap[0] = val &
+		(CAP_FS_MASK_B0 | CAP_TO_MASK(CAP_SYS_RESOURCE) |
+		 CAP_TO_MASK(CAP_LINUX_IMMUTABLE));
+	mdt->mdt_enable_cap_mask.cap[1] = (val >> 32) & CAP_FS_MASK_B1;
+#else
+	mdt->mdt_enable_cap_mask.val = val &
+		(CAP_FS_MASK | BIT_ULL(CAP_SYS_RESOURCE) |
+		 BIT_ULL(CAP_LINUX_IMMUTABLE));
+#endif
+
+	return count;
+}
+LUSTRE_RW_ATTR(enable_cap_mask);
+
 static ssize_t enable_remote_dir_gid_show(struct kobject *kobj,
 					  struct attribute *attr, char *buf)
 {
@@ -1396,6 +1445,7 @@ static struct attribute *mdt_attrs[] = {
 	&lustre_attr_identity_upcall.attr,
 	&lustre_attr_identity_flush.attr,
 	&lustre_attr_evict_tgt_nids.attr,
+	&lustre_attr_enable_cap_mask.attr,
 	&lustre_attr_enable_remote_dir.attr,
 	&lustre_attr_enable_remote_dir_gid.attr,
 	&lustre_attr_enable_chprojid_gid.attr,

lustre/tests/sanity-sec.sh
@@ -4063,6 +4063,13 @@ test_51() {
 		skip "Need MDS version at least 2.13.53"
 
 	mkdir $DIR/$tdir || error "mkdir $tdir"
+	local mdts=$(comma_list $(mdts_nodes))
+	local cap_param=mdt.*.enable_cap_mask
+
+	old_cap=($(do_nodes $mdts $LCTL get_param -n $cap_param 2>/dev/null))
+	do_nodes $mdts $LCTL set_param $cap_param=0xf 2>/dev/null))
+	[[ -z "$old_cap" ]] ||
+		stack_trap "do_nodes $mdts $LCTL set_param $cap_param=$old_cap"
 
 	touch $DIR/$tdir/$tfile || error "touch $tfile"
 	cp $(which chown) $DIR/$tdir || error "cp chown"