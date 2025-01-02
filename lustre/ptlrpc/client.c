995d727b58cc555b62afa542c6897c86985c55c3
Gian-Carlo DeFazio <defazio1@llnl.gov>
2025-01-02 14:24:45 -0800

    Revert "LU-15808 ptlrpc: wait condition should not sleep"
    This reverts commit e06dca47257e85bc5b9b14323652e01b24818c4f.

--- lustre/ptlrpc/client.c
+++ lustre/ptlrpc/client.c
@@ -1781,41 +1781,15 @@ static inline int ptlrpc_set_producer(struct ptlrpc_request_set *set)
 	RETURN((atomic_read(&set->set_remaining) - remaining));
 }
 
-static int ptl_send_rpc_handler(struct ptlrpc_request *req)
-{
-	struct obd_import *imp = req->rq_import;
-	int rc = ptl_send_rpc(req, 0);
-
-	if (rc == -ENOMEM) {
-		spin_lock(&imp->imp_lock);
-		if (!list_empty(&req->rq_list)) {
-			list_del_init(&req->rq_list);
-			if (atomic_dec_and_test(&imp->imp_inflight))
-				wake_up(&imp->imp_recovery_waitq);
-		}
-		spin_unlock(&imp->imp_lock);
-		ptlrpc_rqphase_move(req, RQ_PHASE_NEW);
-	}
-	if (rc) {
-		DEBUG_REQ(D_HA, req, "send failed: rc = %d", rc);
-		spin_lock(&req->rq_lock);
-		req->rq_net_err = 1;
-		spin_unlock(&req->rq_lock);
-	}
-	return rc;
- }
-
 /**
- * this sends (or queues) any unsent RPCs in \a set and returns 1 if all are
- * sent and no more replies are expected.
+ * this sends any unsent RPCs in \a set and returns 1 if all are sent
+ * and no more replies are expected.
  * (it is possible to get less replies than requests sent e.g. due to timed out
  * requests or requests that we had trouble to send out)
  *
  * NOTE: This function contains a potential schedule point (cond_resched()).
  */
-static int ptlrpc_check_set_common(const struct lu_env *env,
-				   struct ptlrpc_request_set *set,
-				   struct list_head *pending)
+int ptlrpc_check_set(const struct lu_env *env, struct ptlrpc_request_set *set)
 {
 	struct ptlrpc_request *req, *next;
 	LIST_HEAD(comp_reqs);
@@ -1844,8 +1818,7 @@ static int ptlrpc_check_set_common(const struct lu_env *env,
 		 * Since the processing time is unbounded, we need to insert an
 		 * explicit schedule point to make the thread well-behaved.
 		 */
-		if (!pending)
-			cond_resched();
+		cond_resched();
 
 		/*
 		 * If the caller requires to allow to be interpreted by force
@@ -2108,17 +2081,22 @@ static int ptlrpc_check_set_common(const struct lu_env *env,
 				    !ptlrpc_unregister_bulk(req, 1))
 					continue;
 
-				if (pending) {
-					/* caller will send */
-					list_move_tail(&req->rq_set_chain,
-						       pending);
+				rc = ptl_send_rpc(req, 0);
+				if (rc == -ENOMEM) {
+					spin_lock(&imp->imp_lock);
+					if (!list_empty(&req->rq_list))
+						list_del_init(&req->rq_list);
+					spin_unlock(&imp->imp_lock);
+					ptlrpc_rqphase_move(req, RQ_PHASE_NEW);
 					continue;
 				}
-
-				rc = ptl_send_rpc_handler(req);
 				if (rc) {
-					if (rc != -ENOMEM)
-						force_timer_recalc = 1;
+					DEBUG_REQ(D_HA, req,
+						  "send failed: rc = %d", rc);
+					force_timer_recalc = 1;
+					spin_lock(&req->rq_lock);
+					req->rq_net_err = 1;
+					spin_unlock(&req->rq_lock);
 					continue;
 				}
 				/* need to reset the timeout */
@@ -2285,42 +2263,9 @@ interpret:
 	list_splice(&comp_reqs, &set->set_requests);
 
 	/* If we hit an error, we want to recover promptly. */
-	RETURN(atomic_read(&set->set_remaining) == 0 ||
-	       (pending && !list_empty(pending)) ||
-	       force_timer_recalc);
-}
-
-/**
- * this queues any unsent RPCs in \a set and returns 1:
- *  - if all are sent and no more replies are expected.
- *
- * NOTE: the caller is reponsible for sending the queued requests
- *       and adding the request back to the set.
- *
- * NOTE: This function contains a potential schedule point (cond_resched()).
- */
-int ptlrpc_check_set(const struct lu_env *env, struct ptlrpc_request_set *set)
-{
-	might_sleep();
-
-	return ptlrpc_check_set_common(env, set, NULL);
+	RETURN(atomic_read(&set->set_remaining) == 0 || force_timer_recalc);
 }
 EXPORT_SYMBOL(ptlrpc_check_set);
-
-/**
- * this queues any unsent RPCs in \a set and returns 1:
- *  - if all are sent and no more replies are expected.
- *  - the pending queue is not empty
- *
- * NOTE: the caller is reponsible for sending the queued requests
- *       and adding the request back to the set.
- */
-static int ptlrpc_check_set_nosleep(const struct lu_env *env,
-				    struct ptlrpc_request_set *set,
-				    struct list_head *pending)
-{
-	return ptlrpc_check_set_common(env, set, pending);
-}
 
 /**
  * Time out request \a req. is \a async_unlink is set, that means do not wait
@@ -2512,57 +2457,6 @@ time64_t ptlrpc_set_next_timeout(struct ptlrpc_request_set *set)
 	RETURN(timeout);
 }
 
-static inline
-int ptlrpc_wait_abortable(time64_t timeout, struct ptlrpc_request_set *set)
-{
-	struct ptlrpc_request *req, *next;
-	LIST_HEAD(pending);
-	int rc;
-	bool sending;
-
-	do {
-		sending = false;
-		rc = l_wait_event_abortable_timeout(
-			set->set_waitq,
-			ptlrpc_check_set_nosleep(NULL, set, &pending),
-			cfs_time_seconds(timeout ? timeout : 1));
-		list_for_each_entry_safe(req, next, &pending, rq_set_chain) {
-			sending = true;
-			ptl_send_rpc_handler(req);
-			list_move_tail(&req->rq_set_chain,
-				       &set->set_requests);
-		}
-	} while (sending);
-
-	return rc;
-}
-
-static inline
-int ptlrpc_wait_idle(time64_t timeout, struct ptlrpc_request_set *set)
-{
-	struct ptlrpc_request *req, *next;
-	LIST_HEAD(pending);
-	int rc;
-	bool sending;
-
-	do {
-		sending = false;
-		rc = wait_event_idle_timeout(
-			set->set_waitq,
-			ptlrpc_check_set_nosleep(NULL, set, &pending),
-			cfs_time_seconds(timeout ? timeout : 1));
-
-		list_for_each_entry_safe(req, next, &pending, rq_set_chain) {
-			sending = true;
-			ptl_send_rpc_handler(req);
-			list_move_tail(&req->rq_set_chain,
-				       &set->set_requests);
-		}
-	} while (sending);
-
-	return rc;
-}
-
 /**
  * Send all unset request from the set and then wait untill all
  * requests in the set complete (either get a reply, timeout, get an
@@ -2605,7 +2499,10 @@ int ptlrpc_set_wait(const struct lu_env *env, struct ptlrpc_request_set *set)
 			 * We still want to block for a limited time,
 			 * so we allow interrupts during the timeout.
 			 */
-			rc = ptlrpc_wait_abortable(timeout, set);
+			rc = l_wait_event_abortable_timeout(
+				set->set_waitq,
+				ptlrpc_check_set(NULL, set),
+				cfs_time_seconds(timeout ? timeout : 1));
 			if (rc == 0) {
 				rc = -ETIMEDOUT;
 				ptlrpc_expired_set(set);
@@ -2621,7 +2518,10 @@ int ptlrpc_set_wait(const struct lu_env *env, struct ptlrpc_request_set *set)
 			 * interrupts are allowed. Wait until all
 			 * complete, or an in-flight req times out.
 			 */
-			rc = ptlrpc_wait_idle(timeout, set);
+			rc = wait_event_idle_timeout(
+				set->set_waitq,
+				ptlrpc_check_set(NULL, set),
+				cfs_time_seconds(timeout ? timeout : 1));
 			if (rc == 0) {
 				ptlrpc_expired_set(set);
 				rc = -ETIMEDOUT;
