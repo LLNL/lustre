ae8e4b545101b3a593e755e66d83d2dee0765409
Alexey Lyashkov <alexey.lyashkov@hpe.com>
2025-04-23 11:23:53 +0300

    LU-18942 obdclass: rework limits for zfs
    ZFS ARC is uncontrolled by Linux memory, so this size should not be
    accounted for all caches, not just the lu_object cache. Let's reduce
    the number of objects freed in a single batch to avoid high CPU usage
    in ARC prune threads and increase latency in providing free space.
    
    Lustre-Change: https://review.whamcloud.com/58918
    Lustre-Commit: TBD
    
    Signed-off-by: Olaf Faaland <faaland1@llnl.gov>
    Test-Parameters: fortestonly
    Test-Parameters: trivial
    HPE-bug-id: LUS-12814, LUS-12813
    Fixes: 79b4ae9139c ("LU-1305 osd: osd_handler")
    Fixes: 0123baecc4e ("LU-5164 osd: Limit lu_object cache")
    Signed-off-by: Alexey Lyashkov <alexey.lyashkov@hpe.com>
    Change-Id: I5342149b185c61c56087d970f26eb4f197a597ef

--- libcfs/libcfs/linux/linux-prim.c
+++ libcfs/libcfs/linux/linux-prim.c
@@ -369,3 +369,22 @@ char *nla_strdup(const struct nlattr *nla, gfp_t flags)
 }
 EXPORT_SYMBOL(nla_strdup);
 #endif /* !HAVE_NLA_STRDUP */
+
+unsigned int libcfs_reserved_cache = 0;
+module_param(libcfs_reserved_cache, int, 0644);
+MODULE_PARM_DESC(libcfs_reserved_cache, "system page cache reservation in mbytes (for arc cache)");
+
+#ifdef HAVE_TOTALRAM_PAGES_AS_FUNC
+  #define _totalram_pages() totalram_pages()
+#else
+  #define _totalram_pages() totalram_pages
+#endif
+
+unsigned long cfs_totalram_pages()
+{
+	if (libcfs_reserved_cache > _totalram_pages()/2)
+		libcfs_reserved_cache = _totalram_pages() / 2;
+
+	return _totalram_pages() - libcfs_reserved_cache;
+}
+EXPORT_SYMBOL(cfs_totalram_pages);
