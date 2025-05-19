lustre/lustre-release

commit 79b4ae9139cba69b2a81db25276cb2c0ae12a31c

LU-1305 osd: osd_handler.c for osd-zfs
functions to setup/mount zfs backend

Signed-off-by: Alex Zhuravlev <bzzz@whamcloud.com>
Change-Id: I200709e0c86e4f7aae35528cbae7f7b08e094f47
Reviewed-on: http://review.whamcloud.com/2963
Tested-by: Hudson
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>

lustre/osd-zfs/osd_handler.c
@@ -1,0 +1,797 @@
+/*
+ * GPL HEADER START
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 only,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License version 2 for more details (a copy is included
+ * in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; If not, see
+ * http://www.sun.com/software/products/lustre/docs/GPLv2.pdf
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa Clara,
+ * CA 95054 USA or visit www.sun.com if you need additional information or
+ * have any questions.
+ *
+ * GPL HEADER END
+ */
+/*
+ * Copyright (c) 2009, 2010, Oracle and/or its affiliates. All rights reserved.
+ * Use is subject to license terms.
+ */
+/*
+ * Copyright (c) 2011, 2012 Whamcloud, Inc.
+ * Use is subject to license terms.
+ *
+ */
+/*
+ * This file is part of Lustre, http://www.lustre.org/
+ * Lustre is a trademark of Sun Microsystems, Inc.
+ *
+ * lustre/osd-zfs/osd_handler.c
+ * Top-level entry points into osd module
+ *
+ * Author: Alex Zhuravlev <bzzz@whamcloud.com>
+ * Author: Mike Pershin <tappro@whamcloud.com>
+ * Author: Johann Lombardi <johann@whamcloud.com>
+ */
+
+#ifndef EXPORT_SYMTAB
+# define EXPORT_SYMTAB
+#endif
+#define DEBUG_SUBSYSTEM S_OSD
+
+#include <lustre_ver.h>
+#include <libcfs/libcfs.h>
+#include <obd_support.h>
+#include <lustre_net.h>
+#include <obd.h>
+#include <obd_class.h>
+#include <lustre_disk.h>
+#include <lustre_fid.h>
+
+#include "osd_internal.h"
+
+#include <sys/dnode.h>
+#include <sys/dbuf.h>
+#include <sys/spa.h>
+#include <sys/stat.h>
+#include <sys/zap.h>
+#include <sys/spa_impl.h>
+#include <sys/zfs_znode.h>
+#include <sys/dmu_tx.h>
+#include <sys/dmu_objset.h>
+#include <sys/dsl_prop.h>
+#include <sys/sa_impl.h>
+#include <sys/txg.h>
+
+struct lu_context_key	osd_key;
+
+static char *root_tag = "osd_mount, rootdb";
+
+/* Slab for OSD object allocation */
+cfs_mem_cache_t *osd_object_kmem;
+
+static struct lu_kmem_descr osd_caches[] = {
+	{
+		.ckd_cache = &osd_object_kmem,
+		.ckd_name  = "zfs_osd_obj",
+		.ckd_size  = sizeof(struct osd_object)
+	},
+	{
+		.ckd_cache = NULL
+	}
+};
+
+static void arc_prune_func(int64_t bytes, void *private)
+{
+	struct osd_device *od = private;
+	struct lu_site    *site = &od->od_site;
+	struct lu_env      env;
+	int rc;
+
+	rc = lu_env_init(&env, LCT_SHRINKER);
+	if (rc) {
+		CERROR("%s: can't initialize shrinker env: rc = %d\n",
+		       od->od_svname, rc);
+		return;
+	}
+
+	lu_site_purge(&env, site, (bytes >> 10));
+
+	lu_env_fini(&env);
+}
+
+/*
+ * Concurrency: doesn't access mutable data
+ */
+static int osd_root_get(const struct lu_env *env,
+			struct dt_device *dev, struct lu_fid *f)
+{
+	lu_local_obj_fid(f, OSD_FS_ROOT_OID);
+	return 0;
+}
+
+/*
+ * OSD object methods.
+ */
+
+/*
+ * Concurrency: shouldn't matter.
+ */
+static void osd_trans_commit_cb(void *cb_data, int error)
+{
+	struct osd_thandle	*oh = cb_data;
+	struct thandle		*th = &oh->ot_super;
+	struct lu_device	*lud = &th->th_dev->dd_lu_dev;
+	struct dt_txn_commit_cb	*dcb, *tmp;
+
+	ENTRY;
+
+	if (error) {
+		if (error == ECANCELED)
+			CWARN("%s: transaction @0x%p was aborted\n",
+			      osd_dt_dev(th->th_dev)->od_svname, th);
+		else
+			CERROR("%s: transaction @0x%p commit error: rc = %d\n",
+				osd_dt_dev(th->th_dev)->od_svname, th, error);
+	}
+
+	dt_txn_hook_commit(th);
+
+	/* call per-transaction callbacks if any */
+	cfs_list_for_each_entry_safe(dcb, tmp, &oh->ot_dcb_list, dcb_linkage)
+		dcb->dcb_func(NULL, th, dcb, error);
+
+	lu_device_put(lud);
+	th->th_dev = NULL;
+	lu_context_exit(&th->th_ctx);
+	lu_context_fini(&th->th_ctx);
+	OBD_FREE_PTR(oh);
+
+	EXIT;
+}
+
+static int osd_trans_cb_add(struct thandle *th, struct dt_txn_commit_cb *dcb)
+{
+	struct osd_thandle *oh;
+
+	oh = container_of0(th, struct osd_thandle, ot_super);
+	cfs_list_add(&dcb->dcb_linkage, &oh->ot_dcb_list);
+
+	return 0;
+}
+
+/*
+ * Concurrency: shouldn't matter.
+ */
+static int osd_trans_start(const struct lu_env *env, struct dt_device *d,
+			   struct thandle *th)
+{
+	struct osd_thandle	*oh;
+	int			rc;
+	ENTRY;
+
+	oh = container_of0(th, struct osd_thandle, ot_super);
+	LASSERT(oh);
+	LASSERT(oh->ot_tx);
+
+	rc = dt_txn_hook_start(env, d, th);
+	if (rc != 0)
+		RETURN(rc);
+
+	if (oh->ot_write_commit && OBD_FAIL_CHECK(OBD_FAIL_OST_MAPBLK_ENOSPC))
+		/* Unlike ldiskfs, ZFS checks for available space and returns
+		 * -ENOSPC when assigning txg */
+		RETURN(-ENOSPC);
+
+	rc = -dmu_tx_assign(oh->ot_tx, TXG_WAIT);
+	if (unlikely(rc != 0)) {
+		struct osd_device *osd = osd_dt_dev(d);
+		/* dmu will call commit callback with error code during abort */
+		if (!lu_device_is_md(&d->dd_lu_dev) && rc == -ENOSPC)
+			CERROR("%s: failed to start transaction due to ENOSPC. "
+			       "Metadata overhead is underestimated or "
+			       "grant_ratio is too low.\n",
+			       osd->od_dt_dev.dd_lu_dev.ld_obd->obd_name);
+		else
+			CERROR("%s: can't assign tx: rc = %d\n",
+			       osd->od_dt_dev.dd_lu_dev.ld_obd->obd_name, rc);
+	} else {
+		/* add commit callback */
+		dmu_tx_callback_register(oh->ot_tx, osd_trans_commit_cb, oh);
+		oh->ot_assigned = 1;
+		lu_context_init(&th->th_ctx, th->th_tags);
+		lu_context_enter(&th->th_ctx);
+		lu_device_get(&d->dd_lu_dev);
+	}
+
+	RETURN(rc);
+}
+
+/*
+ * Concurrency: shouldn't matter.
+ */
+static int osd_trans_stop(const struct lu_env *env, struct thandle *th)
+{
+	struct osd_device	*osd = osd_dt_dev(th->th_dev);
+	struct osd_thandle	*oh;
+	uint64_t		 txg;
+	int			 rc;
+	ENTRY;
+
+	oh = container_of0(th, struct osd_thandle, ot_super);
+
+	if (oh->ot_assigned == 0) {
+		LASSERT(oh->ot_tx);
+		dmu_tx_abort(oh->ot_tx);
+		osd_object_sa_dirty_rele(oh);
+		OBD_FREE_PTR(oh);
+		RETURN(0);
+	}
+
+	rc = dt_txn_hook_stop(env, th);
+	if (rc != 0)
+		CDEBUG(D_OTHER, "%s: transaction hook failed: rc = %d\n",
+		       osd->od_svname, rc);
+
+	LASSERT(oh->ot_tx);
+	txg = oh->ot_tx->tx_txg;
+
+	osd_object_sa_dirty_rele(oh);
+	dmu_tx_commit(oh->ot_tx);
+
+	if (th->th_sync)
+		txg_wait_synced(dmu_objset_pool(osd->od_objset.os), txg);
+
+	RETURN(rc);
+}
+
+static struct thandle *osd_trans_create(const struct lu_env *env,
+					struct dt_device *dt)
+{
+	struct osd_device	*osd = osd_dt_dev(dt);
+	struct osd_thandle	*oh;
+	struct thandle		*th;
+	dmu_tx_t		*tx;
+	ENTRY;
+
+	tx = dmu_tx_create(osd->od_objset.os);
+	if (tx == NULL)
+		RETURN(ERR_PTR(-ENOMEM));
+
+	/* alloc callback data */
+	OBD_ALLOC_PTR(oh);
+	if (oh == NULL) {
+		dmu_tx_abort(tx);
+		RETURN(ERR_PTR(-ENOMEM));
+	}
+
+	oh->ot_tx = tx;
+	CFS_INIT_LIST_HEAD(&oh->ot_dcb_list);
+	CFS_INIT_LIST_HEAD(&oh->ot_sa_list);
+	cfs_sema_init(&oh->ot_sa_lock, 1);
+	th = &oh->ot_super;
+	th->th_dev = dt;
+	th->th_result = 0;
+	th->th_tags = LCT_TX_HANDLE;
+	RETURN(th);
+}
+
+/*
+ * Concurrency: shouldn't matter.
+ */
+int osd_statfs(const struct lu_env *env, struct dt_device *d,
+	       struct obd_statfs *osfs)
+{
+	struct osd_device *osd = osd_dt_dev(d);
+	int		   rc;
+	ENTRY;
+
+	rc = udmu_objset_statfs(&osd->od_objset, osfs);
+	if (rc)
+		RETURN(rc);
+	osfs->os_bavail -= min_t(obd_size,
+				 OSD_GRANT_FOR_LOCAL_OIDS / osfs->os_bsize,
+				 osfs->os_bavail);
+	RETURN(0);
+}
+
+/*
+ * Concurrency: doesn't access mutable data.
+ */
+static void osd_conf_get(const struct lu_env *env,
+			 const struct dt_device *dev,
+			 struct dt_device_param *param)
+{
+	/*
+	 * XXX should be taken from not-yet-existing fs abstraction layer.
+	 */
+	param->ddp_max_name_len  = MAXNAMELEN;
+	param->ddp_max_nlink     = 1 << 31; /* it's 8byte on a disk */
+	param->ddp_block_shift   = 12; /* XXX */
+	param->ddp_mount_type    = LDD_MT_ZFS;
+
+	param->ddp_mntopts        = MNTOPT_USERXATTR | MNTOPT_ACL;
+	param->ddp_max_ea_size    = DXATTR_MAX_ENTRY_SIZE;
+
+	/* for maxbytes, report same value as ZPL */
+	param->ddp_maxbytes      = MAX_LFS_FILESIZE;
+
+	/* Default reserved fraction of the available space that should be kept
+	 * for error margin. Unfortunately, there are many factors that can
+	 * impact the overhead with zfs, so let's be very cautious for now and
+	 * reserve 20% of the available space which is not given out as grant.
+	 * This tunable can be changed on a live system via procfs if needed. */
+	param->ddp_grant_reserved = 20;
+
+	/* inodes are dynamically allocated, so we report the per-inode space
+	 * consumption to upper layers. This static value is not really accurate
+	 * and we should use the same logic as in udmu_objset_statfs() to
+	 * estimate the real size consumed by an object */
+	param->ddp_inodespace = OSD_DNODE_EST_COUNT;
+	/* per-fragment overhead to be used by the client code */
+	param->ddp_grant_frag = udmu_blk_insert_cost();
+}
+
+/*
+ * Concurrency: shouldn't matter.
+ */
+static int osd_sync(const struct lu_env *env, struct dt_device *d)
+{
+	struct osd_device  *osd = osd_dt_dev(d);
+	CDEBUG(D_HA, "syncing OSD %s\n", LUSTRE_OSD_ZFS_NAME);
+	txg_wait_synced(dmu_objset_pool(osd->od_objset.os), 0ULL);
+	return 0;
+}
+
+static int osd_commit_async(const struct lu_env *env, struct dt_device *dev)
+{
+	struct osd_device *osd = osd_dt_dev(dev);
+	tx_state_t	  *tx = &dmu_objset_pool(osd->od_objset.os)->dp_tx;
+	uint64_t	   txg;
+
+	txg = tx->tx_open_txg + 1;
+	if (tx->tx_quiesce_txg_waiting < txg) {
+		tx->tx_quiesce_txg_waiting = txg;
+		cv_broadcast(&tx->tx_quiesce_more_cv);
+	}
+	mutex_exit(&tx->tx_sync_lock);
+
+	return 0;
+}
+
+/*
+ * Concurrency: shouldn't matter.
+ */
+static int osd_ro(const struct lu_env *env, struct dt_device *d)
+{
+	struct osd_device  *osd = osd_dt_dev(d);
+	ENTRY;
+
+	CERROR("%s: *** setting device %s read-only ***\n",
+	       osd->od_svname, LUSTRE_OSD_ZFS_NAME);
+	osd->od_rdonly = 1;
+	spa_freeze(dmu_objset_spa(osd->od_objset.os));
+
+	RETURN(0);
+}
+
+/*
+ * Concurrency: serialization provided by callers.
+ */
+static int osd_init_capa_ctxt(const struct lu_env *env, struct dt_device *d,
+			      int mode, unsigned long timeout, __u32 alg,
+			      struct lustre_capa_key *keys)
+{
+	struct osd_device *dev = osd_dt_dev(d);
+	ENTRY;
+
+	dev->od_fl_capa = mode;
+	dev->od_capa_timeout = timeout;
+	dev->od_capa_alg = alg;
+	dev->od_capa_keys = keys;
+
+	RETURN(0);
+}
+
+static char *osd_label_get(const struct lu_env *env, const struct dt_device *d)
+{
+	struct osd_device *dev = osd_dt_dev(d);
+	int rc;
+	ENTRY;
+
+	rc = -udmu_userprop_get_str(&dev->od_objset, DMU_OSD_SVNAME,
+				    dev->od_svname, sizeof(dev->od_svname));
+	if (rc != 0) {
+		if (rc == -EOVERFLOW)
+			CWARN("%s: buffer too small\n", dev->od_svname);
+		RETURN(NULL);
+	}
+
+	RETURN(&dev->od_svname[0]);
+}
+
+static struct dt_device_operations osd_dt_ops = {
+	.dt_root_get		= osd_root_get,
+	.dt_statfs		= osd_statfs,
+	.dt_trans_create	= osd_trans_create,
+	.dt_trans_start		= osd_trans_start,
+	.dt_trans_stop		= osd_trans_stop,
+	.dt_trans_cb_add	= osd_trans_cb_add,
+	.dt_conf_get		= osd_conf_get,
+	.dt_sync		= osd_sync,
+	.dt_commit_async	= osd_commit_async,
+	.dt_ro			= osd_ro,
+	.dt_init_capa_ctxt	= osd_init_capa_ctxt,
+};
+
+/*
+ * DMU OSD device type methods
+ */
+static int osd_type_init(struct lu_device_type *t)
+{
+	LU_CONTEXT_KEY_INIT(&osd_key);
+	return lu_context_key_register(&osd_key);
+}
+
+static void osd_type_fini(struct lu_device_type *t)
+{
+	lu_context_key_degister(&osd_key);
+}
+
+static void *osd_key_init(const struct lu_context *ctx,
+			  struct lu_context_key *key)
+{
+	struct osd_thread_info *info;
+
+	OBD_ALLOC_PTR(info);
+	if (info != NULL)
+		info->oti_env = container_of(ctx, struct lu_env, le_ctx);
+	else
+		info = ERR_PTR(-ENOMEM);
+	return info;
+}
+
+static void osd_key_fini(const struct lu_context *ctx,
+			 struct lu_context_key *key, void *data)
+{
+	struct osd_thread_info *info = data;
+
+	OBD_FREE_PTR(info);
+}
+
+static void osd_key_exit(const struct lu_context *ctx,
+			 struct lu_context_key *key, void *data)
+{
+	struct osd_thread_info *info = data;
+
+	memset(info, 0, sizeof(*info));
+}
+
+struct lu_context_key osd_key = {
+	.lct_tags = LCT_DT_THREAD | LCT_MD_THREAD | LCT_MG_THREAD | LCT_LOCAL,
+	.lct_init = osd_key_init,
+	.lct_fini = osd_key_fini,
+	.lct_exit = osd_key_exit
+};
+
+static int osd_shutdown(const struct lu_env *env, struct osd_device *o)
+{
+	RETURN(0);
+}
+
+static int osd_mount(const struct lu_env *env,
+		     struct osd_device *o, struct lustre_cfg *cfg)
+{
+	char				*dev  = lustre_cfg_string(cfg, 0);
+	struct lustre_mount_info	*lmi;
+	struct lustre_sb_info		*lsi;
+	dmu_buf_t			*rootdb;
+	char				*label;
+	int				 rc;
+	ENTRY;
+
+	if (o->od_objset.os != NULL)
+		RETURN(0);
+
+	lmi = server_get_mount(dev);
+	if (lmi == NULL) {
+		CERROR("Unknown mount point: '%s'\n", dev);
+		RETURN(-ENODEV);
+	}
+
+	lsi = s2lsi(lmi->lmi_sb);
+	dev = lsi->lsi_lmd->lmd_dev;
+
+	if (strlen(dev) >= sizeof(o->od_mntdev))
+		RETURN(-E2BIG);
+
+	strcpy(o->od_mntdev, dev);
+
+	rc = -udmu_objset_open(o->od_mntdev + 1, &o->od_objset);
+	if (rc) {
+		CERROR("can't open objset %s: %d\n", o->od_mntdev, rc);
+		RETURN(rc);
+	}
+
+	rc = __osd_obj2dbuf(env, o->od_objset.os, o->od_objset.root,
+				&rootdb, root_tag);
+	if (rc) {
+		CERROR("udmu_obj2dbuf() failed with error %d\n", rc);
+		udmu_objset_close(&o->od_objset);
+		RETURN(rc);
+	}
+
+	o->od_root = rootdb->db_object;
+	sa_buf_rele(rootdb, root_tag);
+
+	/* 1. initialize oi before any file create or file open */
+	rc = osd_oi_init(env, o);
+	if (rc)
+		GOTO(err, rc);
+
+	label = osd_label_get(env, &o->od_dt_dev);
+	if (label == NULL)
+		GOTO(err, rc = -ENODEV);
+
+	/* Use our own ZAP for inode accounting by default, this can be changed
+	 * via procfs to estimate the inode usage from the block usage */
+	o->od_quota_iused_est = 0;
+
+	rc = osd_procfs_init(o, label);
+	if (rc)
+		GOTO(err, rc);
+
+	o->arc_prune_cb = arc_add_prune_callback(arc_prune_func, o);
+
+err:
+	RETURN(rc);
+}
+
+static void osd_umount(const struct lu_env *env, struct osd_device *o)
+{
+	ENTRY;
+
+	if (cfs_atomic_read(&o->od_zerocopy_alloc))
+		CERROR("%s: lost %d allocated page(s)\n", o->od_svname,
+		       cfs_atomic_read(&o->od_zerocopy_alloc));
+	if (cfs_atomic_read(&o->od_zerocopy_loan))
+		CERROR("%s: lost %d loaned abuf(s)\n", o->od_svname,
+		       cfs_atomic_read(&o->od_zerocopy_loan));
+	if (cfs_atomic_read(&o->od_zerocopy_pin))
+		CERROR("%s: lost %d pinned dbuf(s)\n", o->od_svname,
+		       cfs_atomic_read(&o->od_zerocopy_pin));
+
+	if (o->od_objset.os != NULL)
+		udmu_objset_close(&o->od_objset);
+
+	EXIT;
+}
+
+static int osd_device_init0(const struct lu_env *env,
+			    struct osd_device *o,
+			    struct lustre_cfg *cfg)
+{
+	struct lu_device	*l = osd2lu_dev(o);
+	int			 rc;
+
+	/* if the module was re-loaded, env can loose its keys */
+	rc = lu_env_refill((struct lu_env *) env);
+	if (rc)
+		GOTO(out, rc);
+
+	l->ld_ops = &osd_lu_ops;
+	o->od_dt_dev.dd_ops = &osd_dt_ops;
+
+	o->od_capa_hash = init_capa_hash();
+	if (o->od_capa_hash == NULL)
+		GOTO(out, rc = -ENOMEM);
+
+out:
+	RETURN(rc);
+}
+
+static struct lu_device *osd_device_alloc(const struct lu_env *env,
+					  struct lu_device_type *t,
+					  struct lustre_cfg *cfg)
+{
+	struct osd_device	*o;
+	int			 rc;
+
+	OBD_ALLOC_PTR(o);
+	if (o == NULL)
+		return ERR_PTR(-ENOMEM);
+
+	rc = dt_device_init(&o->od_dt_dev, t);
+	if (rc == 0) {
+		rc = osd_device_init0(env, o, cfg);
+		if (rc)
+			dt_device_fini(&o->od_dt_dev);
+	}
+
+	if (unlikely(rc != 0))
+		OBD_FREE_PTR(o);
+
+	return rc == 0 ? osd2lu_dev(o) : ERR_PTR(rc);
+}
+
+static struct lu_device *osd_device_free(const struct lu_env *env,
+					 struct lu_device *d)
+{
+	struct osd_device *o = osd_dev(d);
+	ENTRY;
+
+	cleanup_capa_hash(o->od_capa_hash);
+	/* XXX: make osd top device in order to release reference */
+	/*d->ld_site->ls_top_dev = d;
+	lu_site_purge(env, d->ld_site, -1);
+	lu_site_fini(&o->od_site);*/
+	dt_device_fini(&o->od_dt_dev);
+	OBD_FREE_PTR(o);
+
+	RETURN (NULL);
+}
+
+static struct lu_device *osd_device_fini(const struct lu_env *env,
+					 struct lu_device *d)
+{
+	struct osd_device	 *o = osd_dev(d);
+	struct lustre_mount_info *lmi;
+	int rc;
+	ENTRY;
+
+
+	osd_oi_fini(env, o);
+
+	if (o->od_objset.os) {
+		arc_remove_prune_callback(o->arc_prune_cb);
+		o->arc_prune_cb = NULL;
+		osd_sync(env, lu2dt_dev(d));
+		txg_wait_callbacks(spa_get_dsl(dmu_objset_spa(o->od_objset.os)));
+	}
+
+	rc = osd_procfs_fini(o);
+	if (rc) {
+		CERROR("proc fini error %d\n", rc);
+		RETURN(ERR_PTR(rc));
+	}
+
+	if (o->od_objset.os)
+		osd_umount(env, o);
+
+	lmi = server_get_mount_2(o->od_svname);
+	LASSERT(lmi);
+	server_put_mount(lmi->lmi_name, lmi->lmi_mnt);
+
+	RETURN(NULL);
+}
+
+static int osd_device_init(const struct lu_env *env, struct lu_device *d,
+                           const char *name, struct lu_device *next)
+{
+	return 0;
+}
+
+/*
+ * To be removed, setup is performed by osd_device_{init,alloc} and
+ * cleanup is performed by osd_device_{fini,free).
+ */
+static int osd_process_config(const struct lu_env *env,
+			      struct lu_device *d, struct lustre_cfg *cfg)
+{
+	struct osd_device	*o = osd_dev(d);
+	int			 err;
+	ENTRY;
+
+	switch(cfg->lcfg_command) {
+	case LCFG_SETUP:
+		err = osd_mount(env, o, cfg);
+		break;
+	case LCFG_CLEANUP:
+		err = osd_shutdown(env, o);
+		break;
+	default:
+		err = -ENOTTY;
+	}
+
+	RETURN(err);
+}
+
+static int osd_recovery_complete(const struct lu_env *env, struct lu_device *d)
+{
+	ENTRY;
+	RETURN(0);
+}
+
+static int osd_prepare(const struct lu_env *env, struct lu_device *pdev,
+		       struct lu_device *dev)
+{
+	return 0;
+}
+
+struct lu_device_operations osd_lu_ops = {
+	.ldo_object_alloc	= osd_object_alloc,
+	.ldo_process_config	= osd_process_config,
+	.ldo_recovery_complete	= osd_recovery_complete,
+	.ldo_prepare		= osd_prepare,
+};
+
+static void osd_type_start(struct lu_device_type *t)
+{
+}
+
+static void osd_type_stop(struct lu_device_type *t)
+{
+}
+
+static struct lu_device_type_operations osd_device_type_ops = {
+	.ldto_init		= osd_type_init,
+	.ldto_fini		= osd_type_fini,
+
+	.ldto_start		= osd_type_start,
+	.ldto_stop		= osd_type_stop,
+
+	.ldto_device_alloc	= osd_device_alloc,
+	.ldto_device_free	= osd_device_free,
+
+	.ldto_device_init	= osd_device_init,
+	.ldto_device_fini	= osd_device_fini
+};
+
+static struct lu_device_type osd_device_type = {
+	.ldt_tags     = LU_DEVICE_DT,
+	.ldt_name     = LUSTRE_OSD_ZFS_NAME,
+	.ldt_ops      = &osd_device_type_ops,
+	.ldt_ctx_tags = LCT_LOCAL
+};
+
+
+static struct obd_ops osd_obd_device_ops = {
+	.o_owner       = THIS_MODULE,
+};
+
+int __init osd_init(void)
+{
+	int rc;
+
+	rc = osd_options_init();
+	if (rc)
+		return rc;
+
+	rc = lu_kmem_init(osd_caches);
+	if (rc)
+		return rc;
+
+	rc = class_register_type(&osd_obd_device_ops, NULL,
+				 lprocfs_osd_module_vars,
+				 LUSTRE_OSD_ZFS_NAME, &osd_device_type);
+	if (rc)
+		lu_kmem_fini(osd_caches);
+	return rc;
+}
+
+void __exit osd_exit(void)
+{
+	class_unregister_type(LUSTRE_OSD_ZFS_NAME);
+	lu_kmem_fini(osd_caches);
+}
+
+extern unsigned int osd_oi_count;
+CFS_MODULE_PARM(osd_oi_count, "i", int, 0444,
+		"Number of Object Index containers to be created, "
+		"it's only valid for new filesystem.");
+
+MODULE_AUTHOR("Sun Microsystems, Inc. <http://www.lustre.org/>");
+MODULE_DESCRIPTION("Lustre Object Storage Device ("LUSTRE_OSD_ZFS_NAME")");
+MODULE_LICENSE("GPL");
+
+cfs_module(osd, LUSTRE_VERSION_STRING, osd_init, osd_exit);

LLNL/lustre

commit 5e52c034071c5b041d5b25a5fcb832703ec5f532

LU-18942 obdclass: rework limits for zfs
ZFS ARC is uncontrolled by Linux memory, so this size should not be
accounted for all caches, not just the lu_object cache. Let's reduce
the number of objects freed in a single batch to avoid high CPU usage
in ARC prune threads and increase latency in providing free space.

Lustre-Change: https://review.whamcloud.com/58918
Lustre-Commit: TBD

Signed-off-by: Olaf Faaland <faaland1@llnl.gov>
Test-Parameters: fortestonly
Test-Parameters: trivial
HPE-bug-id: LUS-12814, LUS-12813
Fixes: 79b4ae9139c ("LU-1305 osd: osd_handler")
Fixes: 0123baecc4e ("LU-5164 osd: Limit lu_object cache")
Signed-off-by: Alexey Lyashkov <alexey.lyashkov@hpe.com>
Change-Id: I5342149b185c61c56087d970f26eb4f197a597ef

libcfs/include/libcfs/linux/linux-mem.h
@@ -48,15 +48,7 @@
 #include <linux/sched/mm.h>
 #endif
 
-#ifdef HAVE_TOTALRAM_PAGES_AS_FUNC
- #ifndef cfs_totalram_pages
-  #define cfs_totalram_pages() totalram_pages()
- #endif
-#else
- #ifndef cfs_totalram_pages
-  #define cfs_totalram_pages() totalram_pages
- #endif
-#endif
+unsigned long cfs_totalram_pages(void);
 
 #ifndef HAVE_MEMALLOC_RECLAIM
 static inline unsigned int memalloc_noreclaim_save(void)

libcfs/libcfs/linux/linux-prim.c
@@ -369,3 +369,22 @@ char *nla_strdup(const struct nlattr *nla, gfp_t flags)
 }
 EXPORT_SYMBOL(nla_strdup);
 #endif /* !HAVE_NLA_STRDUP */
+
+unsigned int libcfs_reserved_cache = 0;
+module_param(libcfs_reserved_cache, int, 0644);
+MODULE_PARM_DESC(libcfs_reserved_cache, "system page cache reservation in mbytes (for arc cache)");
+
+#ifdef HAVE_TOTALRAM_PAGES_AS_FUNC
+  #define _totalram_pages() totalram_pages()
+#else
+  #define _totalram_pages() totalram_pages
+#endif
+
+unsigned long cfs_totalram_pages()
+{
+	if (libcfs_reserved_cache > _totalram_pages()/2)
+		libcfs_reserved_cache = _totalram_pages() / 2;
+
+	return _totalram_pages() - libcfs_reserved_cache;
+}
+EXPORT_SYMBOL(cfs_totalram_pages);

lustre/include/lu_object.h
@@ -730,6 +730,8 @@ void lu_object_unhash(const struct lu_env *env, struct lu_object *o);
 int lu_site_purge_objects(const struct lu_env *env, struct lu_site *s, int nr,
 			  int canblock);
 
+void lu_site_limit(const struct lu_env *env, struct lu_site *s, u64 limit);
+
 static inline int lu_site_purge(const struct lu_env *env, struct lu_site *s,
 				int nr)
 {

lustre/obdclass/lu_object.c
@@ -81,7 +81,7 @@ enum {
 	LU_CACHE_PERCENT_DEFAULT = 20
 };
 
-#define	LU_CACHE_NR_MAX_ADJUST		512
+#define	LU_CACHE_NR_MAX_ADJUST		1024
 #define	LU_CACHE_NR_UNLIMITED		-1
 #define	LU_CACHE_NR_DEFAULT		LU_CACHE_NR_UNLIMITED
 /** This is set to roughly (20 * OSS_NTHRS_MAX) to prevent thrashing */
@@ -660,23 +660,30 @@ int lu_object_invariant(const struct lu_object *o)
  * maximum number of objects is capped by LU_CACHE_MAX_ADJUST.  This ensures
  * that many concurrent threads will not accidentally purge the entire cache.
  */
-static void lu_object_limit(const struct lu_env *env,
-			    struct lu_device *dev)
+void lu_site_limit(const struct lu_env *env, struct lu_site *s,
+		   u64 nr)
 {
-	u64 size, nr;
+	u64 size;
 
-	if (lu_cache_nr == LU_CACHE_NR_UNLIMITED)
+	if (nr == LU_CACHE_NR_UNLIMITED)
 		return;
 
-	size = atomic_read(&dev->ld_site->ls_obj_hash.nelems);
-	nr = (u64)lu_cache_nr;
+	size = atomic_read(&s->ls_obj_hash.nelems);
 	if (size <= nr)
 		return;
 
-	lu_site_purge_objects(env, dev->ld_site,
+	lu_site_purge_objects(env, s,
 			      min_t(u64, size - nr, LU_CACHE_NR_MAX_ADJUST),
 			      0);
 }
+EXPORT_SYMBOL(lu_site_limit);
+
+static void lu_object_limit(const struct lu_env *env,
+			    struct lu_device *dev)
+{
+	lu_site_limit(env, dev->ld_site, (u64)lu_cache_nr);
+}
+
 
 static struct lu_object *htable_lookup(const struct lu_env *env,
 				       struct lu_device *dev,
@@ -1049,17 +1056,6 @@ EXPORT_SYMBOL(lu_site_print);
 static void lu_htable_limits(struct lu_device *top)
 {
 	unsigned long cache_size;
-
-	/*
-	 * For ZFS based OSDs the cache should be disabled by default.  This
-	 * allows the ZFS ARC maximum flexibility in determining what buffers
-	 * to cache.  If Lustre has objects or buffer which it wants to ensure
-	 * always stay cached it must maintain a hold on them.
-	 */
-	if (strcmp(top->ld_type->ldt_name, LUSTRE_OSD_ZFS_NAME) == 0) {
-		lu_cache_nr = LU_CACHE_NR_ZFS_LIMIT;
-		return;
-	}
 
 	/*
 	 * Calculate hash table size, assuming that we want reasonable

lustre/osd-zfs/osd_handler.c
@@ -107,7 +107,7 @@ static void arc_prune_func(int64_t bytes, void *private)
 		return;
 	}
 
-	lu_site_purge(&env, site, (bytes >> 10));
+	lu_site_limit(&env, site, (bytes >> 10));
 
 	lu_env_fini(&env);
 }