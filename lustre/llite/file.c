6da43dcfc022de31260a890ac0bb9d54800b41e9
Alexey Lyashkov <alexey.lyashkov@hpe.com>
2025-10-24 18:25:14 +0800

    LUS-13048 llite: set read size correctly
    The problem with the extent lock  is that it don't update an i_size
    after lock lost and enqueue new one.  To solve this, vvp_prep_size
    was created.
    However, it can only handle two out of three cases:
    
    1. When the extent lock has been lost and there is no valid size
       available, a glimpse is sent.
    2. When the lock has been renewed and the size has increased,
       the i_size is updated to the minimal known size.
    
    in some cases, the size might decrease after the lock renewal. If
    this happens, it is safe to change the i_size to kms, as the i_size
    value is not accurate anyway. Size can be changed if it's not
    protected by ldlm locks for all stripes, but it don't update an
    i_size. Users should call the stat() function if the size needs to be
    set correctly for this point.
    
    Test sanity 251b was removed because of an incorrect assumption. The
    read after the write should never return an end-of-file (EOF). While
    the test started from a 5-second  sleep before, the write started 2
    seconds after the read starts, so the writer should have finished
    before the read. Once the test was run on the same node, the write
    will update the i_size, and the read would see this update to copy
    data.
    
    Cherry-picked-from: 7eee8d3886315cca099c71dba3e57f6d8589defd
    Cherry-picked-from-change: https://github.com/Cray/lustre/commit/7eee8d3886315
    
    Fixes: 4468f6c9d9 ("LU-16025 llite: adjust read count as file got truncated")
    Signed-off-by: Alexey Lyashkov <alexey.lyashkov@hpe.com>
    Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
    Change-Id: I224b6c6c2fad621e32ab9d9432b4569758e477ab
    Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
    Reviewed-by: Andrew Perepechko <andrew.perepechko@hpe.com>
    Tested-by: Vitaly Fertman <vitaly.fertman@hpe.com>
    Signed-off-by: Gian-Carlo DeFazio <defazio1@llnl.gov>

--- lustre/llite/file.c
+++ lustre/llite/file.c
@@ -1952,59 +1952,6 @@ ll_do_fast_read(struct kiocb *iocb, struct iov_iter *iter)
 	return result;
 }
 
-/**
- * Confine read iter lest read beyond the EOF
- *
- * \param iocb [in]	kernel iocb
- * \param to [in]	reader iov_iter
- *
- * \retval <0	failure
- * \retval 0	success
- * \retval >0	@iocb->ki_pos has passed the EOF
- */
-static int file_read_confine_iter(struct lu_env *env, struct kiocb *iocb,
-				  struct iov_iter *to)
-{
-	struct cl_attr *attr = vvp_env_thread_attr(env);
-	struct file *file = iocb->ki_filp;
-	struct inode *inode = file_inode(file);
-	struct ll_inode_info *lli = ll_i2info(inode);
-	loff_t read_end = iocb->ki_pos + iov_iter_count(to);
-	loff_t kms;
-	loff_t size;
-	int rc;
-
-	cl_object_attr_lock(lli->lli_clob);
-	rc = cl_object_attr_get(env, lli->lli_clob, attr);
-	cl_object_attr_unlock(lli->lli_clob);
-	if (rc != 0)
-		return rc;
-
-	kms = attr->cat_kms;
-	/* if read beyond end-of-file, adjust read count */
-	if (kms > 0 && (iocb->ki_pos >= kms || read_end > kms)) {
-		rc = ll_glimpse_size(inode);
-		if (rc != 0)
-			return rc;
-
-		size = i_size_read(inode);
-		if (iocb->ki_pos >= size || read_end > size) {
-			CDEBUG(D_VFSTRACE,
-			       "%s: read [%llu, %llu] over eof, kms %llu, file_size %llu.\n",
-			       file_dentry(file)->d_name.name,
-			       iocb->ki_pos, read_end, kms, size);
-
-			if (iocb->ki_pos >= size)
-				return 1;
-
-			if (read_end > size)
-				iov_iter_truncate(to, size - iocb->ki_pos);
-		}
-	}
-
-	return rc;
-}
-
 /*
  * Read from a file (through the page cache).
  */
@@ -2013,13 +1960,11 @@ static ssize_t ll_file_read_iter(struct kiocb *iocb, struct iov_iter *to)
 	struct lu_env *env;
 	struct vvp_io_args *args;
 	struct file *file = iocb->ki_filp;
-	loff_t orig_ki_pos = iocb->ki_pos;
 	ssize_t result;
 	ssize_t rc2;
 	__u16 refcheck;
 	ktime_t kstart = ktime_get();
 	bool cached;
-	bool stale_data = false;
 
 	ENTRY;
 
@@ -2034,12 +1979,6 @@ static ssize_t ll_file_read_iter(struct kiocb *iocb, struct iov_iter *to)
 	env = cl_env_get(&refcheck);
 	if (IS_ERR(env))
 		RETURN(PTR_ERR(env));
-
-	result = file_read_confine_iter(env, iocb, to);
-	if (result < 0)
-		GOTO(out, result);
-	else if (result > 0)
-		stale_data = true;
 
 	CFS_FAIL_TIMEOUT_ORSET(OBD_FAIL_LLITE_READ_PAUSE, CFS_FAIL_ONCE,
 			       cfs_fail_val);
@@ -2077,16 +2016,6 @@ static ssize_t ll_file_read_iter(struct kiocb *iocb, struct iov_iter *to)
 
 out:
 	cl_env_put(env, &refcheck);
-
-	if (stale_data && result > 0) {
-		/**
-		 * we've reached EOF before the read, the data read are cached
-		 * stale data.
-		 */
-		iocb->ki_pos = orig_ki_pos;
-		iov_iter_truncate(to, 0);
-		result = 0;
-	}
 
 	if (result > 0) {
 		ll_rw_stats_tally(ll_i2sbi(file_inode(file)), current->pid,
--- lustre/tests/sanity.sh
+++ lustre/tests/sanity.sh
@@ -21668,28 +21668,6 @@ test_251a() {
 }
 run_test 251a "Handling short read and write correctly"
 
-test_251b() {
-	dd if=/dev/zero of=$DIR/$tfile bs=1k count=4 ||
-		error "write $tfile failed"
-
-	sleep 2 && echo 12345 >> $DIR/$tfile &
-
-	#define OBD_FAIL_LLITE_READ_PAUSE 0x1431
-	$LCTL set_param fail_loc=0x1431 fail_val=5
-	# seek to 4096, 2 seconds later, file size expand to 4102, and after
-	# 5 seconds, read 10 bytes, the short read should
-	# report:
-	#                start ->+ read_len -> offset_after_read read_count
-	#     short read: 4096 ->+ 10 -> 4096 0
-	# not:
-	#     short read: 4096 ->+ 10 -> 4102 0
-	local off=$($MULTIOP $DIR/$tfile oO_RDONLY:z4096r10c 2>&1 | \
-			awk '/short read/ { print $7 }')
-	(( off == 4096 )) ||
-		error "short read should set offset at 4096, not $off"
-}
-run_test 251b "short read restore offset correctly"
-
 test_252() {
 	remote_mds_nodsh && skip "remote MDS with nodsh"
 	remote_ost_nodsh && skip "remote OST with nodsh"
