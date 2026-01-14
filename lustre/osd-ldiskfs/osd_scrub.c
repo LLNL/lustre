1771f694ec909c5738d62e4137aadd4683e63361
Shaun Tancheff <shaun.tancheff@hpe.com>
2026-01-14 12:23:37 +0700

    LU-19249 build: Compatibility updates for kernel v6.16
    Linux commit v6.15-rc1-5-g06c567403ae5
      Use try_lookup_noperm() instead of d_hash_and_lookup() outside \
      of VFS
    Provide try_lookup_noperm() and lookup_noperm for older kernels.
    
    Linux commit v6.15-rc6-233-g7d0f0f061531
      mm: move folio_index to mm/swap.h and remove no longer needed \
          helper
    Drop folio_index and use folio->index directly.
    
    Linux commit v6.15-13744-g41cb08555c41
      treewide, timers: Rename from_timer() to timer_container_of()
    Provide timer_container_of for older kernels when is not defined.
    
    Linux commit v6.15-rc6-297-gacc53a0b4c15
      mm: rename page->index to page->__folio_index
    Access page index either via folio->index or calculated using
    folio_index_page()
    
    Cherry-picked-from: 59d84ec3ffb83a938f13e0c93ad5c87ec2a0d258
    Cherry-picked-from-change: https://review.whamcloud.com/60619
    
    Test-Parameters: trivial
    Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
    Change-Id: I25d4eec9a047a6e3aa796d9327d0857842c9c0ba
    Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/60619
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: Timothy Day <timday@amazon.com>
    Tested-by: jenkins <devops@whamcloud.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>
    Reviewed-by: James Simmons <jsimmons@infradead.org>
    Signed-off-by: Gian-Carlo DeFazio <defazio1@llnl.gov>

--- lustre/osd-ldiskfs/osd_scrub.c
+++ lustre/osd-ldiskfs/osd_scrub.c
@@ -1993,7 +1993,7 @@ static int osd_ios_lf_fill(void *buf,
 		RETURN(0);
 
 	scrub->os_lf_scanned++;
-	child = osd_lookup_one_len(dev, name, parent, namelen);
+	child = osd_lookup_noperm(dev, &QSTR_LEN(name, namelen), parent);
 	if (IS_ERR(child)) {
 		rc = PTR_ERR(child);
 		CDEBUG(D_LFSCK, "%s: cannot lookup child '%.*s': rc = %d\n",
@@ -2069,7 +2069,8 @@ static int osd_ios_varfid_fill(void *buf,
 	if (name[0] == '.')
 		RETURN(0);
 
-	child = osd_lookup_one_len(dev, name, fill_buf->oifb_dentry, namelen);
+	child = osd_lookup_noperm(dev, &QSTR_LEN(name, namelen),
+				  fill_buf->oifb_dentry);
 	if (IS_ERR(child))
 		RETURN(PTR_ERR(child));
 
@@ -2118,7 +2119,8 @@ static int osd_ios_dl_fill(void *buf,
 	if (map->olm_name == NULL)
 		RETURN(0);
 
-	child = osd_lookup_one_len(dev, name, fill_buf->oifb_dentry, namelen);
+	child = osd_lookup_noperm(dev, &QSTR_LEN(name, namelen),
+				  fill_buf->oifb_dentry);
 	if (IS_ERR(child))
 		RETURN(PTR_ERR(child));
 
@@ -2153,7 +2155,8 @@ static int osd_ios_uld_fill(void *buf,
 	if (name[0] != '[')
 		RETURN(0);
 
-	child = osd_lookup_one_len(dev, name, fill_buf->oifb_dentry, namelen);
+	child = osd_lookup_noperm(dev, &QSTR_LEN(name, namelen),
+				  fill_buf->oifb_dentry);
 	if (IS_ERR(child))
 		RETURN(PTR_ERR(child));
 
@@ -2204,7 +2207,8 @@ static int osd_ios_root_fill(void *buf,
 	if (map->olm_name == NULL)
 		RETURN(0);
 
-	child = osd_lookup_one_len(dev, name, fill_buf->oifb_dentry, namelen);
+	child = osd_lookup_noperm(dev, &QSTR_LEN(name, namelen),
+				  fill_buf->oifb_dentry);
 	if (IS_ERR(child))
 		RETURN(PTR_ERR(child));
 	else if (!child->d_inode)
@@ -2290,8 +2294,7 @@ osd_ios_ROOT_scan(struct osd_thread_info *info, struct osd_device *dev,
 	spin_lock(&scrub->os_lock);
 	scrub->os_convert_igif = 1;
 	spin_unlock(&scrub->os_lock);
-	child = osd_lookup_one_len_unlocked(dev, dot_lustre_name, dentry,
-					    strlen(dot_lustre_name));
+	child = osd_lookup_noperm_unlocked(dev, &QSTR(dot_lustre_name), dentry);
 	if (IS_ERR(child)) {
 		if (PTR_ERR(child) != -ENOENT)
 			RETURN(PTR_ERR(child));
@@ -2365,8 +2368,7 @@ osd_ios_OBJECTS_scan(struct osd_thread_info *info, struct osd_device *dev,
 			RETURN(rc);
 	}
 
-	child = osd_lookup_one_len_unlocked(dev, ADMIN_USR, dentry,
-					    strlen(ADMIN_USR));
+	child = osd_lookup_noperm_unlocked(dev, &QSTR(ADMIN_USR), dentry);
 	if (IS_ERR(child)) {
 		rc = PTR_ERR(child);
 	} else {
@@ -2381,8 +2383,7 @@ osd_ios_OBJECTS_scan(struct osd_thread_info *info, struct osd_device *dev,
 	if (rc != 0 && rc != -ENOENT)
 		GOTO(out, rc);
 
-	child = osd_lookup_one_len_unlocked(dev, ADMIN_GRP, dentry,
-					    strlen(ADMIN_GRP));
+	child = osd_lookup_noperm_unlocked(dev, &QSTR(ADMIN_GRP), dentry);
 	if (IS_ERR(child))
 		GOTO(out, rc = PTR_ERR(child));
 
@@ -2446,9 +2447,9 @@ static void osd_initial_OI_scrub(struct osd_thread_info *info,
 			continue;
 		}
 
-		child = osd_lookup_one_len_unlocked(dev, map->olm_name,
-						    osd_sb(dev)->s_root,
-						    map->olm_namelen);
+		child = osd_lookup_noperm_unlocked(dev, &QSTR_LEN(map->olm_name,
+						   map->olm_namelen),
+						   osd_sb(dev)->s_root);
 		if (PTR_ERR(child) == -ENOENT ||
 		    (!IS_ERR(child) && !child->d_inode))
 			osd_scrub_refresh_mapping(info, dev, &map->olm_fid,
