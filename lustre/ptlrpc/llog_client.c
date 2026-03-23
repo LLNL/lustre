e795b54bd3cb64e791a2c0a0d33fe5816c0e2851
Alexander Boyko <alexander.boyko@hpe.com>
2026-03-23 15:35:55 +0100

    LU-18570 llog: llog_client error processing
    Fixes llog_client error processing, according to llog_osd_next_block
    EIO -> EBADR change.
    
    Lustre-change: https://review.whamcloud.com/64786
    Lustre-commit: 13eca2cc5ce2a56d4bc04a113c8ccf25d9c1ec2b
    
    Test-Parameters: trivial testlist=conf-sanity env=ONLY=135,ONLY_REPEAT=10
    Fixes: 1a24dcdce121 ("LU-15938 lod: prevent endless retry in recovery thread")
    Signed-off-by: Alexander Boyko <alexander.boyko@hpe.com>
    Change-Id: Ic687b39c5c857d290d554b3077fc64f2ae6e58f4
    Signed-off-by: Gian-Carlo DeFazio <defazio1@llnl.gov>

--- lustre/ptlrpc/llog_client.c
+++ lustre/ptlrpc/llog_client.c
@@ -176,17 +176,19 @@ static int llog_client_next_block(const struct lu_env *env,
 	ptlrpc_request_set_replen(req);
 	rc = ptlrpc_queue_wait(req);
 	/*
-	 * -EIO has a special meaning here. If llog_osd_next_block()
+	 * Older servers use EIO and newer servers use EBADR.
+	 * -EBADR/EIO has a special meaning here. If llog_osd_next_block()
 	 * reaches the end of the log without finding the desired
 	 * record then it updates *cur_offset and *cur_idx and returns
-	 * -EIO. In llog_process_thread() we use this to detect
-	 * EOF. But we must be careful to distinguish between -EIO
-	 * coming from llog_osd_next_block() and -EIO coming from
+	 * -EBADR/EIO. In llog_process_thread() we use this to detect
+	 * EOF. But we must be careful to distinguish between -EBADR
+	 * coming from llog_osd_next_block() and -EBADR coming from
 	 * ptlrpc or below.
 	 */
-	if (rc == -EIO) {
+	if (rc == -EBADR || rc == -EIO) {
 		if (!req->rq_repmsg ||
-		    lustre_msg_get_status(req->rq_repmsg) != -EIO)
+		    (lustre_msg_get_status(req->rq_repmsg) != -EBADR &&
+		     lustre_msg_get_status(req->rq_repmsg) != -EIO))
 			GOTO(out, rc);
 	} else if (rc < 0) {
 		GOTO(out, rc);
